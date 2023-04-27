e43f589ab85796b2d7729cac9c51190fea2ccda5
justurn <justurn@live.com>
2023-04-27 19:59:49 +0800

    raid tweaks

---
+++ custom/db_world/raid tweaks/Onyxia_40/zz_mob_onyxian_whelp.sql
@@ -0,0 +1,8 @@
+-- Taken from lvl80 whelp 11262
+
+DELETE FROM `creature_template` WHERE (`entry` = 301001);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(301001, 36566, 0, 0, 0, 0, 397, 0, 0, 0, 'Onyxian Whelp 60', '', '', 0, 56, 57, 2, 16, 0, 1.11111, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 0, 2048, 8, 0, 0, 0, 0, 0, 2, 8, 0, 0, 100001, 0, 0, 0, 0, '', 1, 1, 5, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 11723);
+
+
+
---
+++ custom/db_world/zz_starting_spells.sql
@@ -0,0 +1,6 @@
+REPLACE INTO playercreateinfo_spell_custom (racemask,classmask,Spell,Note)
+    VALUES 
+    (0,0,8613,'Skinning'),
+    (0,0,2575,'Mining'),
+    (0,0,2366,'Herbalism'),
+    (0,0,514,'Counterspell');
