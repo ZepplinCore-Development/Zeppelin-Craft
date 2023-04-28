4cc2b27f1432983ff019a57849549658f2c1b30f
justurn <justurn@live.com>
2023-04-28 21:12:37 +0800

    updated ubuntu script

--- Scripts/ubuntustartup.sh
+++ Scripts/ubuntustartup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gnome-terminal\
 	--tab\
-		--title="World Server" -- bash -c "cd; cd Zeppelin-Core; git pull; cd; cp -r Zeppelin-Core/custom azerothcore-wotlk/data/sql; azerothcore-wotlk/acore.sh run-worldserver; $SHELL"
+		--title="World Server" -- bash -c "cd; cd Zeppelin-Core; git pull; cd; cp -r Zeppelin-Core/custom azerothcore-wotlk/data/sql; cd; azerothcore-wotlk/acore.sh run-worldserver; $SHELL"
 gnome-terminal\
 	--tab\
 		--title="Auth Server" -- bash -c "azerothcore-wotlk/acore.sh run-authserver; $SHELL"
