a442df74217de339c8e0e6e90e6ee588c77ac7e5
justurn <justurn@live.com>
2023-04-29 12:18:26 +0800

    fixed BWL dragoon loot tables

--- custom/db_world/Raids/BWL/zz_loot_flamegor.sql
+++ custom/db_world/Raids/BWL/zz_loot_flamegor.sql
@@ -1,5 +1,5 @@
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 11981);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(11981, 30337, 30341, 100, 0, 1, 0, 1, 1, NULL),
+(11981, 30337, 30337, 100, 0, 1, 0, 1, 1, NULL),
 (11981, 30342, 30342, 100, 0, 1, 0, 1, 1, NULL),
 (11981, 900000, 900000, 100, 0, 1, 0, 1, 1, NULL);
