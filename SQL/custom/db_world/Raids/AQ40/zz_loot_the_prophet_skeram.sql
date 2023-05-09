10df03330389f7817798e951a390a9c11e7b4a18
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2023-05-09 18:32:20 +0800

    nerfed damage slightly on skeram

--- SQL/custom/db_world/Raids/AQ40/zz_loot_the_prophet_skeram.sql
+++ SQL/custom/db_world/Raids/AQ40/zz_loot_the_prophet_skeram.sql
@@ -1,3 +1,8 @@
+DELETE FROM `creature_template` WHERE (`entry` = 15263);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(15263, 0, 0, 0, 0, 0, 15345, 0, 0, 0, 'The Prophet Skeram', '', NULL, 0, 63, 63, 0, 16, 0, 2.4, 2.14286, 1, 1, 20, 1, 3, 0, 10.05, 2000, 2000, 1, 1, 2, 64, 2048, 0, 0, 0, 0, 0, 0, 10, 76, 15263, 0, 0, 0, 0, 504000, 609840, '', 0, 1, 175, 150, 1.3, 1, 0, 192, 1, 617299807, 0, 0, 'boss_skeram', 12340);
+
+
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 15263);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (15263, 21128, 0, 10, 0, 1, 1, 1, 1, 'The Prophet Skeram - Staff of the Qiraji Prophets'),
