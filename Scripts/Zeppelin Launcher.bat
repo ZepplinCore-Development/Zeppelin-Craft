cde5b5cc14525199ff6780debeee85642d86ef74
justurn <justurn@live.com>
2023-05-11 11:58:05 +0100

    finished launcher script

--- Scripts/Zeppelin Launcher.bat
+++ Scripts/Zeppelin Launcher.bat
@@ -12,30 +12,11 @@ if exist "%var%\Custom Tools\Zeppelin-Core" (
 cd "%var%\Custom Tools\Zeppelin-Core" git pull
 ) else (cd "%var%\Custom Tools" && git clone https://github.com/justurn/Zeppelin-Core)
 
-cmd /k
+:: Copy the exe file downloaded from Zeppelin-Core Github to the client folder.
+xcopy /s /y "%var%\Custom Tools\Zeppelin-Core\EXE Patch\Wow.exe" "%var%"
 
-:: Replace the wow.exe with Zeppelin Cores
+:: Copy the MPQ file downloaded from Zeppelin-Core Github to the client data folder.
+xcopy /s /y "%var%\Custom Tools\Zeppelin-Core\MPQ Patches\patch-5.MPQ" "%var%\Data"
 
-:: Copy Patch-5.MPQ to the Data folder
-
-:: OLD STUFF
-
-:: Clean and pull the latest version of the repo.
-::cd C:\Games\ChromieCraft_3.3.5a\Custom Tools\Zeppelin-Core
-::git pull
-
-:: Copies the DBC files from the spell editor export folder to the server.
-::xcopy /s /y "C:\Games\ChromieCraft_3.3.5a\Custom Tools\WoW Spell Editor\Export" "Y:\env\dist\bin\dbc"
-
-:: Run the MPQ editor to update the local MPQ file (patch-5.MPQ) with the DBC files from the spell editor export folder.
-::cd C:\Games\ChromieCraft_3.3.5a\Custom Tools\MPQ Editor
-::call MPQEditor.exe /console "C:\Games\ChromieCraft_3.3.5a\Custom Tools\Zeppelin-Core\Scripts\BUILD-MPQ5.txt"
-
-:: Copy the updated MPQ file (patch-5.MPQ) to the client Data folder
-::xcopy /s /y "C:\Games\ChromieCraft_3.3.5a\Custom Tools\Zeppelin-Core\MPQ Patches\patch-5.MPQ" "C:\Games\ChromieCraft_3.3.5a\Data"
-
-:: Upload the new patch-5.MPQ file to Github 
-::cd C:\Games\ChromieCraft_3.3.5a\Custom Tools\Zeppelin-Core
-::git add -a
-::git commit -am "Automatic update of patch-5.MPQ"
-::git push
+:: Runs Wow.exe
+start Wow.exe
