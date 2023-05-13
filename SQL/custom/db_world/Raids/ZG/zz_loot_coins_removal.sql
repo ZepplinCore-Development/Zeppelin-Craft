6d2dfcafdbe29c253dc1486e8b89feffcf5a7f72
justurn <justurn@live.com>
2023-05-13 11:55:33 +0800

    Create zz_loot_coins_removal.sql

---
+++ SQL/custom/db_world/Raids/ZG/zz_loot_coins_removal.sql
@@ -0,0 +1,9 @@
+DELETE FROM `item_template` WHERE (`entry` = 19698);
+DELETE FROM `item_template` WHERE (`entry` = 19699);
+DELETE FROM `item_template` WHERE (`entry` = 19700);
+DELETE FROM `item_template` WHERE (`entry` = 19704);
+DELETE FROM `item_template` WHERE (`entry` = 19705);
+DELETE FROM `item_template` WHERE (`entry` = 19706);
+
+DELETE FROM `quest_template` WHERE (`ID` = 8195);
+DELETE FROM `quest_template` WHERE (`ID` = 8239);
