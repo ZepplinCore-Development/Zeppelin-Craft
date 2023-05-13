6ce179ab091ce83a579aa5a5365e428db8ba625a
justurn <justurn@live.com>
2023-05-13 20:04:48 +0800

    Automatic update of patch-5.MPQ

--- SQL/custom/db_world/Raids/ZG/zz_loot_coins_removal.sql
+++ SQL/custom/db_world/Raids/ZG/zz_loot_coins_removal.sql
@@ -4,6 +4,10 @@ DELETE FROM `item_template` WHERE (`entry` = 19700);
 DELETE FROM `item_template` WHERE (`entry` = 19704);
 DELETE FROM `item_template` WHERE (`entry` = 19705);
 DELETE FROM `item_template` WHERE (`entry` = 19706);
+DELETE FROM `item_template` WHERE (`entry` = 19701);
+DELETE FROM `item_template` WHERE (`entry` = 19702);
+DELETE FROM `item_template` WHERE (`entry` = 19703);
 
 DELETE FROM `quest_template` WHERE (`ID` = 8195);
 DELETE FROM `quest_template` WHERE (`ID` = 8239);
+DELETE FROM `quest_template` WHERE (`ID` = 8238);
