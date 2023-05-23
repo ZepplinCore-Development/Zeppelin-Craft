3bcff319aa027cfb059378952efe120ceacdcfc3
justurn <justurn@live.com>
2023-05-23 20:26:12 +0800

    Automatic update of patch-z.MPQ

--- Scripts/Client Scripts/Admin Dependencies.bat
+++ Scripts/Client Scripts/Admin Dependencies.bat
@@ -5,5 +5,7 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 :: Installs GIT
-choco install git
+choco install git -y
 
+:: Installs Wget
+choco install wget -y
--- Scripts/Client Scripts/Zeppelin Launcher.bat
+++ Scripts/Client Scripts/Zeppelin Launcher.bat
@@ -16,7 +16,10 @@ cd "%var%\Custom Tools\Zeppelin-Core" git pull
 xcopy /s /y "%var%\Custom Tools\Zeppelin-Core\EXE Patch\Wow.exe" "%var%"
 
 :: Copy the MPQ file downloaded from Zeppelin-Core Github to the client data folder.
-xcopy /s /y "%var%\Custom Tools\Zeppelin-Core\MPQ Patches\patch-5.MPQ" "%var%\Data"
+xcopy /s /y "%var%\Custom Tools\Zeppelin-Core\MPQ Patches\patch-z.MPQ" "%var%\Data"
+
+:: Download the creature MPQ
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Wp2_1IBnrD_ffTJ1gztK3_prW87gRdUo' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Wp2_1IBnrD_ffTJ1gztK3_prW87gRdUo" -O PATCH-F.MPQ && rm -rf /tmp/cookies.txt
 
 :: Runs Wow.exe
 start Wow.exe
