669ebb46af9508dc804488925b52ae93a7f2b015
justurn <justurn@live.com>
2023-05-29 19:10:45 +0800

    Create zz_spawn_racial_swapper.sql

---
+++ SQL/custom/db_world/Creatures/Spawn/zz_spawn_racial_swapper.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature` WHERE (`id1` = 98888);
+INSERT INTO `creature` (`id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+(98888,0,0,0,0,0,1,1,0,-14442.9,504.177,21.7669,4.01218,300,0.0,0,12600,0,0,0,0,0,'',NULL);
