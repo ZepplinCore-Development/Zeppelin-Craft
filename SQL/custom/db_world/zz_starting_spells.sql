8c26df25ad68151e76080018f53531a499072b9c
justurn <justurn@live.com>
2023-07-13 08:07:16 +0800

    Update zz_starting_spells.sql

--- SQL/custom/db_world/zz_starting_spells.sql
+++ SQL/custom/db_world/zz_starting_spells.sql
@@ -3,4 +3,12 @@ REPLACE INTO playercreateinfo_spell_custom (racemask,classmask,Spell,Note)
     (0,0,8613,'Skinning'),
     (0,0,2575,'Mining'),
     (0,0,2366,'Herbalism'),
+    (0,0,2018,'blacksmithing'),
+    (0,0,2259,'alchemy'),   
+    (0,0,2108,'leatherworking'),
+    (0,0,3908,'tailoring'),
+    (0,0,4036,'engineering'),
+    (0,0,7411,'enchanting'),
+    (0,0,25229,'jewelcrafting'),
+    (0,0,45357,'inscription'),
     (0,0,514,'Counterspell');
