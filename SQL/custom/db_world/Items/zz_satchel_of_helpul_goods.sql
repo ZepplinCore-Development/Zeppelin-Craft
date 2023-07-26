085387c79314a004c247ff29e17fd7a1f6df4568
justurn <justurn@live.com>
2023-07-26 12:04:38 +0800

    Classic Emblems

---
+++ SQL/custom/db_world/Items/zz_satchel_of_helpul_goods.sql
@@ -0,0 +1,7 @@
+DELETE FROM `item_loot_template` WHERE (`Entry` = 52003);
+INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(52003, 1, 10050, 100, 0, 1, 1, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
+(52003, 2, 10051, 100, 0, 1, 2, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
+(52003, 3, 10052, 100, 0, 1, 3, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
+(52003, 4, 10053, 100, 0, 1, 4, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
+(52003, 900104, 0, 100, 0, 1, 0, 1, 1, '');
