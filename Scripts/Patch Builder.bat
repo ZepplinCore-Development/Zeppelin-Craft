09b1d5f19bc1b9f0f7ac6e815a639ec16ecccd65
justurn <justurn@live.com>
2023-08-26 20:15:28 +0800

    Automatic update of PATCH-Z.MPQ

--- Scripts/Patch Builder.bat
+++ Scripts/Patch Builder.bat
@@ -6,7 +6,8 @@ echo script is running here %base_directory%
 
 :: file list and temp files for storage
 set "temp_file=%base_directory%\temp.txt"
-set "file_list=\\NASUS\appdata\binhex-nginx\nginx\MPQ\mandatory\mandatory_file_list.txt"
+:: This needs the appdata directory mapped to Z drive
+set "file_list=Z:\binhex-nginx\nginx\MPQ\mandatory\mandatory_file_list.txt"
 
 :: Runs a headless export from the spell editor, pulling all the DBC files from the MYSQL database, this includes edits made by other devs.
 :: Any DBC edits made outside of the spell editor tool should be saved in the spell editor export folder.
@@ -18,7 +19,8 @@ cd /d "%base_directory%\Zeppelin-Core"
 git pull
 
 :: Copies the DBC files from the spell editor export folder to the server.
-xcopy /s /y "%base_directory%\WoW Spell Editor\Export" "Y:\env\dist\bin\dbc"
+:: This needs the directory mapped to Y drive
+xcopy /s /y "%base_directory%\WoW Spell Editor\Export" "R:\env\dist\bin\dbc"
 
 :: Run the MPQ editor to update the local MPQ file (PATCH-Z.MPQ) with the DBC files from the spell editor export folder.
 cd /d "%base_directory%\MPQ Editor"
