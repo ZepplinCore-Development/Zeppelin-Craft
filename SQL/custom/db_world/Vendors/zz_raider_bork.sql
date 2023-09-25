48f3fe2af0fe6b2d24c403ae1c4062a857ef9e77
justurn <justurn@live.com>
2023-09-25 20:52:58 +0800

    Create zz_raider_bork.sql

---
+++ SQL/custom/db_world/Vendors/zz_raider_bork.sql
@@ -0,0 +1,6 @@
+DELETE FROM `npc_vendor` WHERE (`entry` = 12796);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(12796, 0, 18245, 0, 0, 3001, NULL),
+(12796, 0, 18246, 0, 0, 3001, NULL),
+(12796, 0, 18247, 0, 0, 3001, NULL),
+(12796, 0, 18248, 0, 0, 3001, NULL);
