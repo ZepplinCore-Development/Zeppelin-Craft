1a73c73191a113d59fff1247cd09f8d4b9e3f1b8
justurn <justurn@live.com>
2023-09-26 18:59:04 +0800

    Create zz_spawn_tier_vendors.sql

---
+++ SQL/custom/db_world/Creatures/Spawn/zz_spawn_tier_vendors.sql
@@ -0,0 +1,7 @@
+DELETE FROM `creature` WHERE (`id1` = 9000010);
+DELETE FROM `creature` WHERE (`id1` = 9000011);
+DELETE FROM `creature` WHERE (`id1` = 9000012);
+INSERT INTO `creature` (`id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+(9000012,0,0,531,0,0,1,1,0,-8203.28,2002.19,132.381,2.63284,300,0.0,0,10080,8814,0,0,0,0,'',NULL),
+(9000011,0,0,469,0,0,1,1,0,-7622.12,-1095.21,407.206,2.15634,300,0.0,0,10080,8814,0,0,0,0,'',NULL),
+(9000010,0,0,409,0,0,1,1,0,1062.93,-491.835,-108.7,0.639821,300,0.0,0,10080,8814,0,0,0,0,'',NULL);
