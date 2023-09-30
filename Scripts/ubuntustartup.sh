d1aeb5a6a9b6cf37011e5015b938deb7fe445575
justurn <justurn@live.com>
2023-09-30 14:29:50 +0800

    Add files via upload

--- Scripts/ubuntustartup.sh
+++ Scripts/ubuntustartup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+sleep 5
+
 gnome-terminal\
 	--tab\
 		--title="World Server" -- bash -c "cd; cd Zeppelin-Core; git pull; cd; cd azerothcore-wotlk/data/sql; rm -r custom; cd; cp -r Zeppelin-Core/SQL/custom azerothcore-wotlk/data/sql; cd; azerothcore-wotlk/acore.sh run-worldserver; $SHELL"
 gnome-terminal\
 	--tab\
 		--title="Auth Server" -- bash -c "cd; azerothcore-wotlk/acore.sh run-authserver; $SHELL"
+		
+sleep 5
+		
 gnome-terminal\
 	--tab\
 		--title="Registration Bot" -- bash -c "cd; cd registrationbot && npm start; $SHELL"
