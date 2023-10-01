63eed3713cee9ef71c865c43b9cba0eddf9ab3ed
justurn <justurn@live.com>
2023-10-01 13:47:27 +0800

    Create zz_heroic_creatures.sql

---
+++ SQL/custom/db_world/Dungeons/Stockades/zz_heroic_creatures.sql
@@ -0,0 +1,12 @@
+-- Defias Prisoner
+REPLACE INTO creature_template VALUES (9100000, 0, 0, 0, 0, 0, 2148, 0, 0, 0, Defias Prisoner, None, None, 0, 23, 24, 0, 290, 0, 1.0, 1.14286, 1.0, 1.0, 20.0, 1.0, 1, 0, 1.7, 2000, 2000, 1.0, 1.0, 1, 32832, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 1706, 1706, 0, 0, 0, 64, 323, SmartAI, 1, 1.0, 3.0, 1.0, 1.0, 1.0, 0, 0, 1, 0, 0, 0, , 12340);
+UPDATE creature_template SET difficulty_entry_1 = 9100000 WHERE entry = 1706;
+-- Defias Captive
+REPLACE INTO creature_template VALUES (9100001, 0, 0, 0, 0, 0, 2144, 0, 0, 0, Defias Captive, None, None, 0, 23, 24, 0, 17, 0, 1.0, 1.14286, 1.0, 1.0, 20.0, 1.0, 1, 0, 1.7, 2000, 2000, 1.0, 1.0, 1, 32832, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 1707, 1707, 0, 0, 0, 60, 325, SmartAI, 1, 1.0, 3.0, 1.0, 1.0, 1.0, 0, 0, 1, 0, 0, 0, , 12340);
+UPDATE creature_template SET difficulty_entry_1 = 9100001 WHERE entry = 1707;
+-- Defias Inmate
+REPLACE INTO creature_template VALUES (9100002, 0, 0, 0, 0, 0, 2146, 0, 0, 0, Defias Inmate, None, None, 0, 24, 25, 0, 17, 0, 1.0, 1.14286, 1.0, 1.0, 20.0, 1.0, 1, 0, 1.7, 2000, 2000, 1.0, 1.0, 1, 32832, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 1708, 1708, 0, 0, 0, 64, 369, SmartAI, 1, 1.0, 3.0, 1.0, 1.0, 1.0, 0, 0, 1, 0, 0, 0, , 12340);
+UPDATE creature_template SET difficulty_entry_1 = 9100002 WHERE entry = 1708;
+-- Defias Convict
+REPLACE INTO creature_template VALUES (9100003, 0, 0, 0, 0, 0, 2145, 0, 0, 0, Defias Convict, None, None, 0, 24, 25, 0, 17, 0, 1.0, 1.14286, 1.0, 1.0, 20.0, 1.0, 1, 0, 1.7, 2000, 2000, 1.0, 1.0, 1, 32832, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 1711, 1711, 0, 0, 0, 74, 348, SmartAI, 1, 1.0, 3.0, 1.0, 1.0, 1.0, 0, 0, 1, 0, 0, 0, , 12340);
+UPDATE creature_template SET difficulty_entry_1 = 9100003 WHERE entry = 1711;
