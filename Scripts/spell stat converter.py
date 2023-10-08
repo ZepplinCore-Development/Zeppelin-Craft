7da6935151a95195b4b2fbd14c24d13d135b0edd
justurn <justurn@live.com>
2023-10-08 12:27:20 +0800

    Update spell stat converter.py

--- Scripts/spell stat converter.py
+++ Scripts/spell stat converter.py
@@ -11,10 +11,11 @@ db_config = {
 # Define the spell_id to search for
 spell_id_to_search = 14056
 
-# Define the new values for stat_type10, stat_value10, spell_id1, and StatsCount increment
+# Define the new values for stat_type10, stat_value10, spell_id1, spell_id2, and StatsCount increment
 new_stat_type10 = 38
 new_stat_value10 = 50
 new_spell_id1 = 0
+new_spell_id2 = 0
 stats_count_increment = 1  # Adjust this value as needed
 
 try:
@@ -27,18 +28,24 @@ try:
         # Create a cursor object to interact with the database
         cursor = connection.cursor()
 
-        # Search for entries where spellid_1 is equal to spell_id_to_search
-        cursor.execute("SELECT entry, stat_type10 FROM item_template WHERE spellid_1 = %s", (spell_id_to_search,))
+        # Search for entries where spellid_1 or spell_id2 is equal to spell_id_to_search
+        cursor.execute("SELECT entry, name, stat_type10, spellid_1, spellid_2 FROM item_template WHERE spellid_1 = %s OR spellid_2 = %s", (spell_id_to_search, spell_id_to_search))
 
-        # Fetch and print the 'entry' and 'stat_type10' values
+        # Fetch and print the 'entry', 'stat_type10', 'spell_id1', and 'spell_id2' values
         records = cursor.fetchall()
         for record in records:
             entry = record[0]
-            stat_type10 = record[1]
+            name = record[1]
+            stat_type10 = record[2]
+            spellid_1 = record[3]
+            spellid_2 = record[4]
             
             if stat_type10 == 0:
-                print(f"Formed query for updating entry {entry}:")
-                update_query = f"UPDATE item_template SET stat_type10 = {new_stat_type10}, stat_value10 = {new_stat_value10}, spellid_1 = {new_spell_id1}, StatsCount = StatsCount + {stats_count_increment} WHERE entry = {entry};"
+                print(f"--{name}")
+                if spellid_1 == spell_id_to_search:
+                    update_query = f"UPDATE `item_template` SET `stat_type10` = {new_stat_type10}, `stat_value10` = {new_stat_value10}, `spellid_1` = {new_spell_id1}, `StatsCount` = StatsCount + {stats_count_increment} WHERE `entry` = {entry};"
+                elif spellid_2 == spell_id_to_search:
+                    update_query = f"UPDATE `item_template` SET `stat_type10` = {new_stat_type10}, `stat_value10` = {new_stat_value10}, `spellid_2` = {new_spell_id2}, `StatsCount` = StatsCount + {stats_count_increment} WHERE `entry` = {entry};"
                 print(update_query)
 
 except mysql.connector.Error as err:
