091103615645dbab7532135cf53f9302a84ee1b2
justurn <justurn@live.com>
2023-10-09 21:30:31 +0800

    Update spell stat converter.py

--- Scripts/spell stat converter.py
+++ Scripts/spell stat converter.py
@@ -1,75 +1,105 @@
 import mysql.connector
+import os
 
-# Database configuration
-db_config = {
-    "host": "192.168.0.99",
-    "user": "keira",
-    "password": "slipknot9",
-    "database": "acore_world"
-}
+# Define the full path of the output file
+output_file_path = "C:/Games/ChromieCraft_3.3.5a/Custom Tools/Zeppelin-Core/SQL/custom/db_world/spell_to_stat.sql"
 
-# Define the list of spell IDs to search for
-spell_ids_to_search = [9136, 9137, 9138, 9139, 9140, 9141, 9142, 9329, 9330, 9331, 9332, 14027, 9334, 9335, 9336, 15807, 15806, 14089, 15808, 14049, 15809, 15810, 15811, 9333, 14056, 15812, 15813, 15814, 15815, 14052, 15816, 15817, 15818, 15819, 15820, 15821, 15823, 15824, 15825, 15826, 15827, 15828, 15829, 15830, 15831, 15832, 29524, 33485, 28840, 28735, 42107, 36068, 34509, 33782, 40933, 48015, 42039, 39885]  # Replace with your list of spell IDs
+# Check if the output directory exists and create it if it doesn't
+output_dir = os.path.dirname(output_file_path)
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
+# Check if the output file exists and delete it if it does
+if os.path.exists(output_file_path):
+    os.remove(output_file_path)
+    print(f"Deleted existing {output_file_path}")
 
-# Define the stat_name
-stat_name = "attack power"  # Replace with the actual stat name
+# Open the output file in write mode
+with open(output_file_path, "w") as output_file:
 
-# Define the initial values and increment for stat_value10
-starting_value = 2
-value_increment = 2
+    # Database configuration for acore_world database
+    db_config_acore_world = {
+        "host": "192.168.0.99",
+        "user": "keira",
+        "password": "slipknot9",
+        "database": "acore_world"
+    }
 
-# Define the new values for stat_type10, stat_value10, spell_id1, spell_id2, and StatsCount increment
-new_stat_type10 = 38
-new_spell_id = 0
-stats_count_increment = 1  # Adjust this value as needed
+    # Connect to the acore_world database to look up values
+    try:
+        connection_acore_world = mysql.connector.connect(**db_config_acore_world)
 
-try:
-    # Connect to the MySQL server
-    connection = mysql.connector.connect(**db_config)
+        # Create a cursor for the acore_world database
+        cursor_acore_world = connection_acore_world.cursor()
 
-    if connection.is_connected():
-        print("Connected to MySQL database")
+        # Query to fetch values from db_Spell_12340 table
+        cursor_acore_world.execute("SELECT ID, EffectBasePoints_1, Name_Lang_enUS, stat_type FROM db_Spell_12340")
 
-        # Create a cursor object to interact with the database
-        cursor = connection.cursor()
+        # Fetch all rows from the table
+        spell_rows = cursor_acore_world.fetchall()
 
-        # Loop through each spell ID in the list
-        for spell_id_to_search in spell_ids_to_search:
+        # Loop through each row in db_Spell_12340 table
+        for spell_row in spell_rows:
+            spell_id_to_search = spell_row[0]
+            stat_value = spell_row[1]
+            stat_name = spell_row[2]
+            new_stat_type10 = spell_row[3]
 
-            # Print the stat_name along with the current value_increment
-            print(f"-- +{starting_value} {stat_name}")
+            # output the spell name and value
+            output_query = f"-- {stat_name}: +{stat_value}: SPELL ({spell_id_to_search})\n\n\n"
+            output_file.write(output_query)
 
-            # Search for entries where spellid_1, spellid_2, or spellid_3 is equal to spell_id_to_search
-            cursor.execute("SELECT entry, name, stat_type10, spellid_1, spellid_2, spellid_3 FROM item_template WHERE spellid_1 = %s OR spellid_2 = %s OR spellid_3 = %s", (spell_id_to_search, spell_id_to_search, spell_id_to_search))
+            # Database configuration for item_template table
+            db_config_item_template = {
+                "host": "192.168.0.99",
+                "user": "keira",
+                "password": "slipknot9",
+                "database": "acore_world"
+            }
 
-            # Fetch and print the 'entry', 'name', 'stat_type10', 'spellid_1', 'spellid_2', and 'spellid_3' values
-            records = cursor.fetchall()
+            # Connect to the acore_world database for item_template table
+            connection_item_template = mysql.connector.connect(**db_config_item_template)
+
+            # Create a cursor for the item_template database
+            cursor_item_template = connection_item_template.cursor()
+
+            # Search for entries where any of the spellid columns is equal to spell_id_to_search
+            cursor_item_template.execute(
+                "SELECT entry, name, stat_type10, spellid_1, spellid_2, spellid_3, spellid_4, spellid_5, "
+                "spelltrigger_1, spelltrigger_2, spelltrigger_3, spelltrigger_4, spelltrigger_5 FROM item_template WHERE "
+                "spellid_1 = %s OR spellid_2 = %s OR spellid_3 = %s OR spellid_4 = %s OR spellid_5 = %s",
+                (spell_id_to_search, spell_id_to_search, spell_id_to_search, spell_id_to_search, spell_id_to_search))
+            
+            # Fetch and write the relevant values to the output file
+            records = cursor_item_template.fetchall()
             for record in records:
                 entry = record[0]
                 name = record[1]
                 stat_type10 = record[2]
-                spellid_1 = record[3]
-                spellid_2 = record[4]
-                spellid_3 = record[5]
-                
-                if stat_type10 == 0:
-                    print(f"--{name}")
-                    if spellid_1 == spell_id_to_search:
-                        update_query = f"UPDATE item_template SET stat_type10 = {new_stat_type10}, stat_value10 = {starting_value}, spellid_1 = {new_spell_id}, StatsCount = StatsCount + {stats_count_increment} WHERE entry = {entry};"
-                    elif spellid_2 == spell_id_to_search:
-                        update_query = f"UPDATE item_template SET stat_type10 = {new_stat_type10}, stat_value10 = {starting_value}, spellid_2 = {new_spell_id}, StatsCount = StatsCount + {stats_count_increment} WHERE entry = {entry};"
-                    elif spellid_3 == spell_id_to_search:
-                        update_query = f"UPDATE item_template SET stat_type10 = {new_stat_type10}, stat_value10 = {starting_value}, spellid_3 = {new_spell_id}, StatsCount = StatsCount + {stats_count_increment} WHERE entry = {entry};"
-                    print(update_query)
+                spellids = record[3:8]  # Get all spellid columns into a list
+                spelltriggers = record[8:13]  # Get all spelltrigger columns into a list
 
-            # Increment the starting_value for the next spell
-            starting_value += value_increment
+                # Find the index of the targeted spellid in the list of spellids
+                spellid_index = spellids.index(spell_id_to_search)
 
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection closed")
+                # Check if stat_type10 is equal to 0 and the associated spelltrigger is equal to 1
+                if stat_type10 == 0 and spelltriggers[spellid_index] == 1:
+                    output_query = f"--{name}\n"
+                    output_query += f"UPDATE item_template SET stat_type10 = {new_stat_type10}, " \
+                                    f"stat_value10 = {stat_value}, spellid_{spellid_index + 1} = {spell_id_to_search} " \
+                                    f"WHERE entry = {entry};\n\n"
+                    # Write the update query to the output file
+                    output_file.write(output_query)
+
+            # Close the cursor and connection for item_template database
+            cursor_item_template.close()
+            connection_item_template.close()
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        if connection_acore_world.is_connected():
+            cursor_acore_world.close()
+            connection_acore_world.close()
+
+print(f"Update queries have been written to {output_file_path}")
