ceff502f4ff71049968df5dc39c4b3d209d5c904
justurn <justurn@live.com>
2023-10-10 19:21:19 +0800

    Update spell stat converter.py

--- Scripts/spell stat converter.py
+++ Scripts/spell stat converter.py
@@ -2,7 +2,7 @@ import mysql.connector
 import os
 
 # Define the full path of the output file
-output_file_path = "C:\Games\ChromieCraft_3.3.5a\Custom Tools\Item Builder\spell_to_stat.sql"
+output_file_path = "C:/Games/ChromieCraft_3.3.5a/Custom Tools/Zeppelin-Core/SQL/custom/db_world/update_queries.sql"
 
 # Check if the output directory exists and create it if it doesn't
 output_dir = os.path.dirname(output_file_path)
@@ -48,10 +48,6 @@ with open(output_file_path, "w") as output_file:
             stat_name = spell_row[2]
             new_stat_type = spell_row[3]
 
-            # output the spell name and value
-            output_query = f"-- {stat_name}: +{stat_value}: SPELL ({spell_id_to_search})\n\n\n"
-            output_file.write(output_query)
-
             # Database configuration for item_template table (reuse the same configuration)
             db_config_item_template = db_config_acore_world.copy()
 
@@ -64,10 +60,11 @@ with open(output_file_path, "w") as output_file:
             # Search for entries where any of the spellid columns is equal to spell_id_to_search
             cursor_item_template.execute(
                 "SELECT entry, name, spellid_1, spellid_2, spellid_3, spellid_4, spellid_5, "
+                "spelltrigger_1, spelltrigger_2, spelltrigger_3, spelltrigger_4, spelltrigger_5, "
                 "stat_type1, stat_type2, stat_type3, stat_type4, stat_type5, "
                 "stat_type6, stat_type7, stat_type8, stat_type9, stat_type10, "
                 "stat_value1, stat_value2, stat_value3, stat_value4, stat_value5, "
-                "stat_value6, stat_value7, stat_value8, stat_value9, stat_value10 "
+                "stat_value6, stat_value7, stat_value8, stat_value9, stat_value10, StatsCount "
                 "FROM item_template WHERE "
                 "spellid_1 = %s OR spellid_2 = %s OR spellid_3 = %s OR spellid_4 = %s OR spellid_5 = %s",
                 (spell_id_to_search, spell_id_to_search, spell_id_to_search, spell_id_to_search, spell_id_to_search))
@@ -78,8 +75,10 @@ with open(output_file_path, "w") as output_file:
                 entry = record[0]
                 name = record[1]
                 spell_ids = record[2:7]  # Get all spellid columns into a list
-                stat_types = record[7:17]  # Get all stat_type columns into a list
-                stat_values = record[17:27]  # Get all stat_value columns into a list
+                spell_triggers = record[7:12]  # Get all spelltrigger columns into a list
+                stat_types = record[12:22]  # Get all stat_type columns into a list
+                stat_values = record[22:32]  # Get all stat_value columns into a list
+                stats_count = record[32]  # Get StatsCount
 
                 # Create a list to track used stat slots for this item
                 used_stat_slots = []
@@ -104,18 +103,25 @@ with open(output_file_path, "w") as output_file:
                     # Mark the slot as used
                     used_stat_slots.append(empty_stat_index + 1)  # Add 1 to convert to 1-based indexing
 
-                    # Store the updated used_stat_slots list in the dictionary
-                    used_stat_slots_dict[entry] = used_stat_slots
-
                     # Prepare the SET clause for the SQL query
                     set_clause = ", ".join([f"stat_type{empty_stat_index + 1} = {stat_types[empty_stat_index]}",
                                             f"stat_value{empty_stat_index + 1} = {stat_values[empty_stat_index]}"])
 
-                    # Generate and write the update query to the output file
-                    output_query = f"--{name} (Used Stat Slots: {', '.join(map(str, used_stat_slots))})\n"
-                    output_query += f"UPDATE item_template SET {set_clause} " \
-                                    f"WHERE entry = {entry};\n\n"
-                    output_file.write(output_query)
+                    # Check if the corresponding spelltrigger is 1 for the spell we are targeting
+                    if spell_triggers[spell_ids.index(spell_id_to_search)] == 1:
+                        # Increment StatsCount only when spelltrigger_x is 1
+                        stats_count_increment = 1
+
+                        # Prepare the SET clause for StatsCount
+                        set_stats_count_clause = f"StatsCount = StatsCount + {stats_count_increment}"
+
+                        # Generate and write the update query to the output file
+                        output_query = f"--{name} (Used Stat Slots: {', '.join(map(str, used_stat_slots))})\n"
+                        output_query += f"UPDATE item_template SET {set_clause}, "
+                        output_query += f"spellid_{spell_ids.index(spell_id_to_search) + 1} = 0, "  # Set spellid_x to 0
+                        output_query += f"{set_stats_count_clause} "  # Increment StatsCount
+                        output_query += f"WHERE entry = {entry};\n\n"
+                        output_file.write(output_query)
 
                 # Close the cursor and connection for item_template database
                 cursor_item_template.close()
