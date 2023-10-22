68462db2e8495897edbcc6e1a53d8e9f25abc725
justurn <justurn@live.com>
2023-10-22 13:38:36 +0800

    Create zz_achievement_criteria_data.sql

---
+++ SQL/custom/db_world/zz_achievement_criteria_data.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
+	(13500,12,1,0,''),
+	(13501,12,1,0,''),
+	(13502,12,1,0,''),
+	(13503,12,1,0,'');
