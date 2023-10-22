83f469f6cd9a67f34e60431afd8d6957711ee4c7
justurn <justurn@live.com>
2023-10-22 13:38:29 +0800

    Create zz_heroic_instance_encounters.sql

---
+++ SQL/custom/db_world/Dungeons/zz_heroic_instance_encounters.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `instance_encounters` (entry,creditType,creditEntry,lastEncounterDungeon,comment) VALUES
+	 (1012,0,7358,298,'Amnennar the Coldbringer'),
+	 (1008,0,4421,297,'Charlga Razorflank'),
+	 (1002,0,4275,296,'Archmage Arugal'),
+	 (991,0,1716,295,'Bazil Thredd');
