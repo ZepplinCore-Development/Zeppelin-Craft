66e9ada9a4dc51aca74bf1c4d0b9d1bc610ad3c3
justurn <justurn@live.com>
2023-10-25 08:05:15 +0800

    Everything Has Been Moved!

---
+++ SQL/custom/db_world/Zones/Eastern Kingdoms/Tiristfal Glades/zz_awful_experiments.sql
@@ -0,0 +1,31 @@
+DELETE FROM `quest_template` WHERE (`ID` = 90001);
+INSERT INTO `quest_template` (`ID`, `QuestType`, `QuestLevel`, `MinLevel`, `QuestSortID`, `QuestInfoID`, `SuggestedGroupNum`, `RequiredFactionId1`, `RequiredFactionId2`, `RequiredFactionValue1`, `RequiredFactionValue2`, `RewardNextQuest`, `RewardXPDifficulty`, `RewardMoney`, `RewardMoneyDifficulty`, `RewardBonusMoney`, `RewardDisplaySpell`, `RewardSpell`, `RewardHonor`, `RewardKillHonor`, `StartItem`, `Flags`, `RequiredPlayerKills`, `RewardItem1`, `RewardAmount1`, `RewardItem2`, `RewardAmount2`, `RewardItem3`, `RewardAmount3`, `RewardItem4`, `RewardAmount4`, `ItemDrop1`, `ItemDropQuantity1`, `ItemDrop2`, `ItemDropQuantity2`, `ItemDrop3`, `ItemDropQuantity3`, `ItemDrop4`, `ItemDropQuantity4`, `RewardChoiceItemID1`, `RewardChoiceItemQuantity1`, `RewardChoiceItemID2`, `RewardChoiceItemQuantity2`, `RewardChoiceItemID3`, `RewardChoiceItemQuantity3`, `RewardChoiceItemID4`, `RewardChoiceItemQuantity4`, `RewardChoiceItemID5`, `RewardChoiceItemQuantity5`, `RewardChoiceItemID6`, `RewardChoiceItemQuantity6`, `POIContinent`, `POIx`, `POIy`, `POIPriority`, `RewardTitle`, `RewardTalents`, `RewardArenaPoints`, `RewardFactionID1`, `RewardFactionValue1`, `RewardFactionOverride1`, `RewardFactionID2`, `RewardFactionValue2`, `RewardFactionOverride2`, `RewardFactionID3`, `RewardFactionValue3`, `RewardFactionOverride3`, `RewardFactionID4`, `RewardFactionValue4`, `RewardFactionOverride4`, `RewardFactionID5`, `RewardFactionValue5`, `RewardFactionOverride5`, `TimeAllowed`, `AllowableRaces`, `LogTitle`, `LogDescription`, `QuestDescription`, `AreaDescription`, `QuestCompletionLog`, `RequiredNpcOrGo1`, `RequiredNpcOrGo2`, `RequiredNpcOrGo3`, `RequiredNpcOrGo4`, `RequiredNpcOrGoCount1`, `RequiredNpcOrGoCount2`, `RequiredNpcOrGoCount3`, `RequiredNpcOrGoCount4`, `RequiredItemId1`, `RequiredItemId2`, `RequiredItemId3`, `RequiredItemId4`, `RequiredItemId5`, `RequiredItemId6`, `RequiredItemCount1`, `RequiredItemCount2`, `RequiredItemCount3`, `RequiredItemCount4`, `RequiredItemCount5`, `RequiredItemCount6`, `Unknown0`, `ObjectiveText1`, `ObjectiveText2`, `ObjectiveText3`, `ObjectiveText4`, `VerifiedBuild`) VALUES
+(90001, 2, 14, 10, 153, 0, 0, 0, 0, 0, 0, 0, 5, 143, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31268, 1, 31270, 1, 31269, 1, 0, 0, 0, 0, 0, 0, 0, 1698, 125, 0, 0, 0, 0, 118, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 690, 'Awful Experiments', 'Eradicate Failed Abominations from the ruins of Lordaeron.', 'Deathguard George is seeking skilled adventurers to aid in the eradication of abominations within the Lordaeron ruins. These monstrous creations are the result of failed experiments by the Forsaken and are causing havoc and destruction. They are concentrated on the northern side of the ruins, closest to the entrance to Undercity. George is offering a reward for any abominations killed, and warns that they are highly dangerous and should not be underestimated. Can you help rid the ruins of these grotesque creatures?', '', 'Return to Deathguard George to claim your reward. He is at the Forsaken camp at the south side of Lordaeron ruins.', 10564, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Failed Abominations', '', '', '', 0);
+
+DELETE FROM `quest_template_addon` WHERE (`ID` = 90001);
+INSERT INTO `quest_template_addon` (`ID`, `MaxLevel`, `AllowableClasses`, `SourceSpellID`, `PrevQuestID`, `NextQuestID`, `ExclusiveGroup`, `RewardMailTemplateID`, `RewardMailDelay`, `RequiredSkillID`, `RequiredSkillPoints`, `RequiredMinRepFaction`, `RequiredMaxRepFaction`, `RequiredMinRepValue`, `RequiredMaxRepValue`, `ProvidedItemCount`, `SpecialFlags`) VALUES
+(90001, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+DELETE FROM `quest_offer_reward` WHERE (`ID` = 90001);
+INSERT INTO `quest_offer_reward` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `EmoteDelay4`, `RewardText`, `VerifiedBuild`) VALUES
+(90001, 0, 0, 0, 0, 0, 0, 0, 0, 'clearing out the abomination mess within the ruins is just the beginning. Please choose your reward.', 0);
+
+DELETE FROM `quest_request_items` WHERE (`ID` = 90001);
+INSERT INTO `quest_request_items` (`ID`, `EmoteOnComplete`, `EmoteOnIncomplete`, `CompletionText`, `VerifiedBuild`) VALUES
+(90001, 0, 0, 'Return to me once you have dealt with the abominations in the ruins', 0);
+
+DELETE FROM `creature_queststarter` WHERE (`quest` = 90001) AND (`id` IN (9000002));
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
+(9000002, 90001);
+
+DELETE FROM `creature_questender` WHERE (`quest` = 90001) AND (`id` IN (9000002));
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
+(9000002, 90001);
+
+DELETE FROM `quest_poi` WHERE (`QuestID` = 90001);
+INSERT INTO `quest_poi` (`QuestID`,`id`,`ObjectiveIndex`,`MapID`,`WorldMapAreaId`,`Floor`,`Priority`,`Flags`,`VerifiedBuild`)
+    VALUES (90001,0,0,0,3601,0,0,1,0);
+
+DELETE FROM `quest_poi_points` WHERE (`QuestID` = 90001);
+INSERT INTO `quest_poi_points` (`QuestID`,`Idx1`,`Idx2`,`X`,`Y`,`VerifiedBuild`)
+    VALUES (90001,0,0,1698,125,0);
---
+++ SQL/custom/db_world/Zones/Kalimdor/Feralas/Dire Maul/zz_loot_King_Gordok.sql
@@ -0,0 +1,13 @@
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 11501);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(11501, 14047, 0, 13, 0, 1, 0, 2, 5, 'King Gordok - Runecloth'),
+(11501, 18332, 0, 2, 0, 1, 1, 1, 1, 'King Gordok - Libram of Rapidity'),
+(11501, 18333, 0, 2, 0, 1, 1, 1, 1, 'King Gordok - Libram of Focus'),
+(11501, 18334, 0, 2, 0, 1, 1, 1, 1, 'King Gordok - Libram of Protection'),
+(11501, 18640, 0, 2, 0, 1, 0, 1, 1, 'King Gordok - Happy Fun Rock'),
+(11501, 18780, 0, 9, 0, 1, 0, 1, 1, 'King Gordok - Top Half of Advanced Armorsmithing: Volume I'),
+(11501, 19258, 0, 7, 0, 1, 0, 1, 1, 'King Gordok - Ace of Warlords'),
+(11501, 21982, 0, 80, 1, 1, 0, 1, 1, 'King Gordok - Ogre Warbeads'),
+(11501, 35016, 35016, 2, 0, 1, 1, 1, 1, 'King Gordok - (ReferenceTable)'),
+(11501, 35019, 35019, 100, 0, 1, 1, 1, 2, 'King Gordok - (ReferenceTable)'),
+(11501, 91000, 0, 4, 0, 1, 0, 1, 1, '');
