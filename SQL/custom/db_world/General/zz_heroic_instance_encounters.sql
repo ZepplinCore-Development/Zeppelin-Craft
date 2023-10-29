d5ff3cacbfb6b743a515b063e78f6e6706cb6217
justurn <justurn@live.com>
2023-10-29 20:28:47 +0800

    Update zz_heroic_instance_encounters.sql

--- SQL/custom/db_world/General/zz_heroic_instance_encounters.sql
+++ SQL/custom/db_world/General/zz_heroic_instance_encounters.sql
@@ -1,4 +1,7 @@
 REPLACE INTO `instance_encounters` (entry,creditType,creditEntry,lastEncounterDungeon,comment) VALUES
+	 (1029,0,7800,350,'Mekgineer Thermaplugg'),
+	 (1014,0,11518,300,'Jergosh the Invoker'),
+	 (1013,0,3654,299,'Mutanus the Devourer'),
 	 (1012,0,7358,298,'Amnennar the Coldbringer'),
 	 (1008,0,4421,297,'Charlga Razorflank'),
 	 (1002,0,4275,296,'Archmage Arugal'),
