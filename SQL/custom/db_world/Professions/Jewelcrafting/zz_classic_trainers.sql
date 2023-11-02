88df7456564cf78c88b6d7a99bd5aa330f839bea
justurn <justurn@live.com>
2023-11-02 18:10:49 +0800

    Create zz_classic_trainers.sql

---
+++ SQL/custom/db_world/Professions/Jewelcrafting/zz_classic_trainers.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature` WHERE (id1 =  19778);
+INSERT INTO `creature` (id1,id2,id3,`map`,zoneId,areaId,spawnMask,phaseMask,equipment_id,position_x,position_y,position_z,orientation,spawntimesecs,wander_distance,currentwaypoint,curhealth,curmana,MovementType,npcflag,unit_flags,dynamicflags,ScriptName,VerifiedBuild,CreateObject,Comment) VALUES
+(19778,0,0,530,0,0,1,1,0,-3781.55,-11541.8,-134.744,1.93941,120,0.0,0,811,852,0,0,0,0,'',0,0,NULL),
+(19778,0,0,0,0,0,1,1,0,-8714.31,620.134,100.927,0.0639622,300,0.0,0,811,852,0,0,0,0,'',NULL,0,NULL);
