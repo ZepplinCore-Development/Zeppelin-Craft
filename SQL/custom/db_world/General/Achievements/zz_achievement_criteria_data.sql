d827092a9c3c494f4bb7df7b00eef8a237a8fad3
justurn <justurn@live.com>
2023-11-05 20:31:57 +0800

    Create zz_achievement_criteria_data.sql

---
+++ SQL/custom/db_world/General/Achievements/zz_achievement_criteria_data.sql
@@ -0,0 +1,45 @@
+-- Azeroth Heroic Dungeons
+REPLACE INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
+(13500,12,1,0,''), -- Shadowfang Keep
+(13501,12,1,0,''), -- Stormwind Stockade
+(13502,12,1,0,''), -- Razorfen Downs
+(13503,12,1,0,''), -- Razorfen Kraul
+(13504,12,1,0,''), -- Wailing Caverns
+(13505,12,1,0,''), -- Ragefire Chasm
+(13506,12,1,0,''), -- Deadmines
+(13507,12,1,0,''), -- Zul'Farrak
+(13508,12,1,0,''), -- Gnomeregan
+(13509,12,1,0,''), -- Blackrock Depths
+(13510,12,1,0,''), -- Upper Blackrock Spire
+(13511,12,1,0,''), -- Lower Blackrock Spire
+(13512,12,1,0,''), -- Blackfathom Deeps
+(13513,12,1,0,''), -- Scarlet Monastery
+(13514,12,1,0,''), -- Uldaman
+(13515,12,1,0,''), -- Maraudon
+(13516,12,1,0,''), -- Sunken Temple
+(13517,12,1,0,''), -- Scholomance
+(13518,12,1,0,''), -- Stratholme
+(13519,12,1,0,''); -- King of Dire Maul
+
+-- Azeroth Mythic Dungeons
+REPLACE INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
+(13600,12,2,0,''), -- Deadmines
+(13601,12,2,0,''), -- Gnomeregan
+(13602,12,2,0,''), -- Shadowfang Keep
+(13603,12,2,0,''), -- Ragefire Chasm
+(13604,12,2,0,''), -- Razorfen Downs
+(13605,12,2,0,''), -- Razorfen Kraul
+(13606,12,2,0,''), -- Shadowfang Keep
+(13607,12,2,0,''), -- Wailing Caverns
+(13608,12,2,0,''), -- Blackrock Depths
+(13609,12,2,0,''), -- Upper Blackrock Spire
+(13610,12,2,0,''), -- Lower Blackrock Spire
+(13611,12,2,0,''), -- Blackfathom Deeps
+(13612,12,2,0,''), -- Scarlet Monastery
+(13613,12,2,0,''), -- Zul'Farrak
+(13614,12,2,0,''), -- Uldaman
+(13615,12,2,0,''), -- Maraudon
+(13616,12,2,0,''), -- Sunken Temple
+(13617,12,2,0,''), -- Scholomance
+(13618,12,2,0,''), -- Stratholme
+(13619,12,2,0,''); -- King of Dire Maul
