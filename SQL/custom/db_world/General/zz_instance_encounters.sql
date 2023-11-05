ffff1c08dcdef4ce34dda794b93f557294aa9e3a
justurn <justurn@live.com>
2023-11-05 21:57:14 +0800

    Create zz_instance_encounters.sql

---
+++ SQL/custom/db_world/General/zz_instance_encounters.sql
@@ -0,0 +1,19 @@
+-- NORMALS
+REPLACE INTO `instance_encounters` (entry,creditType,creditEntry,lastEncounterDungeon,comment) VALUES
+(431,0,11518,4,'Jergosh the Invoker'); -- Ragefire Chasm
+
+-- HEROICS
+REPLACE INTO `instance_encounters` (entry,creditType,creditEntry,lastEncounterDungeon,comment) VALUES
+(1112,0,639,323,'Edwin VanCleef'), -- Deadmines
+(1034,0,7800,350,'Mekgineer Thermaplugg'), -- Gnomeregan
+(1014,0,11518,300,'Jergosh the Invoker'), -- Ragefire Chasm
+(1013,0,3654,299,'Mutanus the Devourer'), -- Wailing Caverns
+(1012,0,7358,298,'Amnennar the Coldbringer'), -- Razorfen Downs
+(1008,0,4421,297,'Charlga Razorflank'), -- Razorfen Kraul
+(1002,0,4275,296,'Archmage Arugal'), -- Shadowfang Keep
+(991,0,1716,295,'Bazil Thredd'); -- Stockades
+
+-- MYTHICS
+REPLACE INTO `instance_encounters` (entry,creditType,creditEntry,lastEncounterDungeon,comment) VALUES
+(1029,0,7800,350,'Mekgineer Thermaplugg'), -- Gnomeregan
+(1105,0,639,351,'Edwin VanCleef'); -- Deadmines
