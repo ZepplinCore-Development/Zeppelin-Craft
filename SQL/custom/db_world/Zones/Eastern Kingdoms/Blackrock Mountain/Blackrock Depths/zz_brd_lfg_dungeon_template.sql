cf6d523ff8819f7865d2e3ef02224cd54db7494e
justurn <justurn@live.com>
2023-11-08 20:23:02 +0800

    Create zz_brd_lfg_dungeon_template.sql

---
+++ SQL/custom/db_world/Zones/Eastern Kingdoms/Blackrock Mountain/Blackrock Depths/zz_brd_lfg_dungeon_template.sql
@@ -0,0 +1,2 @@
+REPLACE INTO acore_world.lfg_dungeon_template (dungeonId,name,position_x,position_y,position_z,orientation,VerifiedBuild) VALUES
+	 (276,'Blackrock Depths - Upper City',881.5,-237.3,-49.9,5.8,0);
