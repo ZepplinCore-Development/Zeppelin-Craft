2e743d12a656e6613c0e297ba03d6b49bcf48ae5
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2023-11-16 19:24:11 +0800

    removed npcs in front of naxx tp

---
+++ SQL/custom/db_world/Zones/Eastern Kingdoms/Eastern Plaguelands/zz_naxx_entrnace_fix.sql
@@ -0,0 +1,17 @@
+DELETE FROM `creature` WHERE (`id1` = 12262);
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(53118, 12262, 0, 0, 0, 0, 0, 1, 1, 0, 3227.72, -3096.21, 161.423, 3.47321, 345, 0, 0, 3589, 0, 0, 0, 0, 0, '', 0, 0, NULL),
+(91941, 12262, 0, 0, 0, 0, 0, 1, 1, 0, 2794.86, -3761.12, 125.507, 3.36848, 345, 0, 0, 3589, 0, 0, 0, 0, 0, '', 0, 0, NULL),
+(91947, 12262, 0, 0, 0, 0, 0, 1, 1, 0, 2862.1, -3232.82, 134.27, 0.034907, 345, 0, 0, 3589, 0, 0, 0, 0, 0, '', 0, 0, NULL),
+(92258, 12262, 0, 0, 0, 0, 0, 1, 1, 0, 3175.24, -3315.21, 165.244, 2.28638, 345, 0, 0, 3589, 0, 0, 0, 0, 0, '', 0, 0, NULL),
+(92266, 12262, 0, 0, 0, 0, 0, 1, 1, 0, 3178.88, -3468.25, 167.479, 4.27606, 345, 0, 0, 3589, 0, 0, 0, 0, 0, '', 0, 0, NULL);
+
+DELETE FROM `creature` WHERE (`id1` = 8553);
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(91940, 8553, 0, 0, 0, 0, 0, 1, 1, 1, 2814.52, -3757.79, 122.507, 3.35103, 345, 0, 0, 2305, 4896, 0, 0, 0, 0, '', 0, 0, NULL),
+(91946, 8553, 0, 0, 0, 0, 0, 1, 1, 1, 2855.31, -3232.34, 134.27, 6.26573, 345, 0, 0, 2305, 4896, 0, 0, 0, 0, '', 0, 0, NULL),
+(92264, 8553, 0, 0, 0, 0, 0, 1, 1, 1, 3185.53, -3456.8, 165.529, 4.13643, 345, 0, 0, 2305, 4896, 0, 0, 0, 0, '', 0, 0, NULL),
+(92269, 8553, 0, 0, 0, 0, 0, 1, 1, 1, 3075.49, -3368.79, 154.443, 0, 345, 0, 0, 2305, 4896, 0, 0, 0, 0, '', 0, 0, NULL),
+(92271, 8553, 0, 0, 0, 0, 0, 1, 1, 1, 3083.83, -3368.92, 154.443, 3.22886, 345, 0, 0, 2305, 4896, 0, 0, 0, 0, '', 0, 0, NULL),
+(92274, 8553, 0, 0, 0, 0, 0, 1, 1, 1, 3259.74, -3390.09, 143.522, 1.71042, 345, 0, 0, 2305, 4896, 0, 0, 0, 0, '', 0, 0, NULL),
+(92279, 8553, 0, 0, 0, 0, 0, 1, 1, 1, 3258.43, -3373.41, 144.274, 4.67748, 345, 0, 0, 2305, 4896, 0, 0, 0, 0, '', 0, 0, NULL);
