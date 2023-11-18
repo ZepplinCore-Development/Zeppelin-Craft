1b4c45933cded59ef26d86aa21e3bc81a41e0962
justurn <justurn@live.com>
2023-11-18 13:09:00 +0800

    Delete zz_spawn_orgrimmar_banker.sql

--- SQL/custom/db_world/Zones/Kalimdor/Orgrimmar/zz_spawn_orgrimmar_banker.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `creature` WHERE (`id1` = 8724);
-INSERT INTO `creature` (`id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(8724,0,0,1,0,0,1,1,0,1667.62,-4463.76,20.1475,1.71042,300,0.0,0,6645,0,0,0,0,0,'',0),
-(8724,0,0,1,0,0,1,1,0,1666.15,-4444.11,19.3163,0.350318,300,0.0,0,5317,0,0,0,0,0,'',NULL);
-
