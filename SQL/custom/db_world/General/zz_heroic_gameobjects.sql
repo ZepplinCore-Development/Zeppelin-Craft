e1f05bd5637915445b1183acb24dcd3e907d8ad4
justurn <justurn@live.com>
2023-12-07 18:13:56 +0800

    Update zz_heroic_gameobjects.sql

--- SQL/custom/db_world/General/zz_heroic_gameobjects.sql
+++ SQL/custom/db_world/General/zz_heroic_gameobjects.sql
@@ -1,3 +1,3 @@
 UPDATE gameobject
 SET spawnMask = 7
-WHERE map = 33 OR map = 34 OR map = 129 OR map = 47 OR map = 43 OR map = 389 OR map = 90;
+WHERE map IN (34, 33, 47, 129, 43, 389, 90, 48, 189, 70, 209, 349, 109, 230, 229, 429, 329, 289, 36);
