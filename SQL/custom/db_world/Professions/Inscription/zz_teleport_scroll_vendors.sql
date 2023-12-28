e15705e2d8c81e74b2c31d6c339d4add6d2bb351
justurn <justurn@live.com>
2023-12-28 18:54:19 +0800

    add more teleport scrolls

--- SQL/custom/db_world/Professions/Inscription/zz_teleport_scroll_vendors.sql
+++ SQL/custom/db_world/Professions/Inscription/zz_teleport_scroll_vendors.sql
@@ -1,51 +1,39 @@
-DELETE FROM `npc_vendor` WHERE (`entry` = 3323);
+-- Horthus - Scroll of Orgrimmar
+DELETE FROM `npc_vendor` WHERE (`entry` = 3323 AND `item` = 900207);
 INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
-(3323, 0, 5565, 0, 0, 0, 0),
-(3323, 0, 16583, 0, 0, 0, 0),
-(3323, 0, 17019, 0, 0, 0, NULL),
-(3323, 0, 17020, 0, 0, 0, 0),
-(3323, 0, 17021, 0, 0, 0, 0),
-(3323, 0, 17026, 0, 0, 0, 0),
-(3323, 0, 17028, 0, 0, 0, 0),
-(3323, 0, 17029, 0, 0, 0, 0),
-(3323, 0, 17030, 0, 0, 0, 0),
-(3323, 0, 17031, 0, 0, 0, 0),
-(3323, 0, 17032, 0, 0, 0, 0),
-(3323, 0, 17033, 0, 0, 0, 0),
-(3323, 0, 17034, 0, 0, 0, 0),
-(3323, 0, 17035, 0, 0, 0, 0),
-(3323, 0, 17036, 0, 0, 0, 0),
-(3323, 0, 17037, 0, 0, 0, 0),
-(3323, 0, 17038, 0, 0, 0, 0),
-(3323, 0, 21177, 0, 0, 0, 0),
-(3323, 0, 900207, 0, 0, 0, 0); -- scroll of Orgrimaar
+(3323, 0, 900207, 0, 0, 0, 0);
 
-DELETE FROM `npc_vendor` WHERE (`entry` = 1307);
+-- Charys Yserian - Scroll of Stormwind
+DELETE FROM `npc_vendor` WHERE (`entry` = 1307 AND `item` = 900208);
 INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
-(1307, 0, 929, 3, 7200, 0, 0),
-(1307, 0, 3385, 2, 9000, 0, 0),
-(1307, 0, 4824, 1, 9000, 0, 0),
-(1307, 0, 4825, 1, 9000, 0, 0),
-(1307, 0, 4826, 1, 9000, 0, 0),
-(1307, 0, 4827, 1, 9000, 0, 0),
-(1307, 0, 4828, 1, 9000, 0, 0),
-(1307, 0, 4829, 1, 9000, 0, 0),
-(1307, 0, 5565, 0, 0, 0, 0),
-(1307, 0, 16583, 0, 0, 0, 0),
-(1307, 0, 17019, 0, 0, 0, NULL),
-(1307, 0, 17020, 0, 0, 0, 0),
-(1307, 0, 17021, 0, 0, 0, 0),
-(1307, 0, 17026, 0, 0, 0, 0),
-(1307, 0, 17028, 0, 0, 0, 0),
-(1307, 0, 17029, 0, 0, 0, 0),
-(1307, 0, 17030, 0, 0, 0, 0),
-(1307, 0, 17031, 0, 0, 0, 0),
-(1307, 0, 17032, 0, 0, 0, 0),
-(1307, 0, 17033, 0, 0, 0, 0),
-(1307, 0, 17034, 0, 0, 0, 0),
-(1307, 0, 17035, 0, 0, 0, 0),
-(1307, 0, 17036, 0, 0, 0, 0),
-(1307, 0, 17037, 0, 0, 0, 0),
-(1307, 0, 17038, 0, 0, 0, 0),
-(1307, 0, 21177, 0, 0, 0, 0),
-(1307, 0, 900208, 0, 0, 0, 0);  -- scroll of Stormwind
+(1307, 0, 900208, 0, 0, 0, 0);
+
+-- Hannah Akeley - Scroll of Undercity
+DELETE FROM `npc_vendor` WHERE (`entry` = 4575 AND `item` = 900212);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(4575, 0, 900212, 0, 0, 0, 0);
+
+-- Barim Jurgenstaad - Scroll of Ironforge
+DELETE FROM `npc_vendor` WHERE (`entry` = 5110 AND `item` = 900210);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(5110, 0, 900210, 0, 0, 0, 0);
+
+-- Alaindia - Scroll of Darnassus
+DELETE FROM `npc_vendor` WHERE (`entry` = 3562 AND `item` = 900209);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(3562, 0, 900209, 0, 0, 0, 0);
+
+-- Zalle - Scroll of Silvermoon City
+DELETE FROM `npc_vendor` WHERE (`entry` = 16611 AND `item` = 900250);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(16611, 0, 900250, 0, 0, 0, 0);
+
+-- Bildine - Scroll of Exodar
+DELETE FROM `npc_vendor` WHERE (`entry` = 16757 AND `item` = 900251);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(16757, 0, 900251, 0, 0, 0, 0);
+
+-- Chepi - Scroll of Thunder Bluff
+DELETE FROM `npc_vendor` WHERE (`entry` = 8361 AND `item` = 900213);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(8361, 0, 900213, 0, 0, 0, 0);
