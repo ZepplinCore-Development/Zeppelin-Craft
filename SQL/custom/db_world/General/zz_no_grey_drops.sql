2bff010c982578054ad1609b4ef63ccc3487d91a
justurn <justurn@live.com>
2024-01-01 20:46:22 +0800

    Update zz_no_grey_drops.sql

--- SQL/custom/db_world/General/zz_no_grey_drops.sql
+++ SQL/custom/db_world/General/zz_no_grey_drops.sql
@@ -1,2 +1,5 @@
 DELETE FROM creature_loot_template 
 WHERE item IN (SELECT entry FROM item_template WHERE quality = 0);
+
+DELETE FROM reference_loot_template 
+WHERE item IN (SELECT entry FROM item_template WHERE quality = 0);
