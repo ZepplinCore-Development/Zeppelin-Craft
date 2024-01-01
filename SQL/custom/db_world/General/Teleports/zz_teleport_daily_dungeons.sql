b91256bc5d81bb1398970098fa22bd7a929dfc35
justurn <justurn@live.com>
2024-01-01 12:42:43 +0800

    Update zz_teleport_daily_dungeons.sql

--- SQL/custom/db_world/General/Teleports/zz_teleport_daily_dungeons.sql
+++ SQL/custom/db_world/General/Teleports/zz_teleport_daily_dungeons.sql
@@ -2,11 +2,11 @@ REPLACE INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`,
 (91018, 0, 349, 752.91, -616.53, -33.11, 1.37, 0), -- Maraudon Purple
 (91019, 0, 349, 1019.69, -458.31, -43.43, 0.31, 0), -- Maraudon Orange
 (91020, 0, 349, 495.70, 17.34, -96.31, 3.12, 0), -- Maraudon Pristine Waters
-(91021, 0, 0, 1610.83, -323.43, 18.67, 6.28, 0), -- Scarlet Monastery - Armory
-(91022, 0, 1, 855.68, 1321.5, 18.67, 0.00, 0), -- Scarlet Monastery - Cathedral
-(91023, 0, 1, 1688.99, 1053.48, 18.68, 0.00, 0), -- Scarlet Monastery - Graveyard
-(91024, 0, 0, 255.35, -209.09, 18.68, 6.27, 0), -- Scarlet Monastery - Library
-(91025, 0, 1, 458.32, 26.52, -70.67, 4.95, 0), -- Blackrock Depths - Prison
-(91026, 0, 0, 881.5, -237.3, -49.9, 5.8, 0), -- Blackrock Depths - Upper City
-(91027, 0, 0, 1214.15, 841.47, 8.93, 6.09, 0), -- Zul'Farrak
-(91028, 0, 0, -319.24, 99.9, -131.85, 3.19, 0); -- Sunken Temple
+(91021, 0, 189, 1610.83, -323.43, 18.67, 6.28, 0), -- Scarlet Monastery - Armory
+(91022, 0, 189, 855.68, 1321.5, 18.67, 0.00, 0), -- Scarlet Monastery - Cathedral
+(91023, 0, 189, 1688.99, 1053.48, 18.68, 0.00, 0), -- Scarlet Monastery - Graveyard
+(91024, 0, 189, 255.35, -209.09, 18.68, 6.27, 0), -- Scarlet Monastery - Library
+(91025, 0, 230, 458.32, 26.52, -70.67, 4.95, 0), -- Blackrock Depths - Prison
+(91026, 0, 230, 881.5, -237.3, -49.9, 5.8, 0), -- Blackrock Depths - Upper City
+(91027, 0, 209, 1214.15, 841.47, 8.93, 6.09, 0), -- Zul'Farrak
+(91028, 0, 109, -319.24, 99.9, -131.85, 3.19, 0); -- Sunken Temple
