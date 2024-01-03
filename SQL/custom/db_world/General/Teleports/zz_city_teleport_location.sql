03646e5e145eab6c015493259075f2dbe11bf5a6
justurn <justurn@live.com>
2024-01-03 19:44:34 +0800

    Create zz_city_teleport_location.sql

---
+++ SQL/custom/db_world/General/Teleports/zz_city_teleport_location.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `Orientation`, `VerifiedBuild`) VALUES
+(3561, 0, 0, -8793.13, 651.03, 95.38, 3.87, 0); -- Stormwind
