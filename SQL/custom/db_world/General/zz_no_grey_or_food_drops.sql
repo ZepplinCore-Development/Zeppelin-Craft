f40248c9739b3d2322eefd3eb8c0ccb4bd4b87b0
justurn <justurn@live.com>
2024-01-05 21:53:15 +0800

    Update zz_no_grey_or_food_drops.sql

--- SQL/custom/db_world/General/zz_no_grey_or_food_drops.sql
+++ SQL/custom/db_world/General/zz_no_grey_or_food_drops.sql
@@ -1,19 +1,50 @@
--- Remove Greys from Creature Loot Table
-DELETE FROM creature_loot_template 
-WHERE item IN (SELECT entry FROM item_template WHERE quality = 0);
+
+-- FOOD
 
 -- Remove Food from Creature Loot Table
 DELETE FROM creature_loot_template 
 WHERE item IN (SELECT entry FROM item_template WHERE (class = 0 AND subclass = 5));
 
--- Remove Greys from Reference Loot Table
-DELETE FROM reference_loot_template 
-WHERE item IN (SELECT entry FROM item_template WHERE quality = 0);
+-- Remove Food from Game Object Loot Table
+DELETE FROM gameobject_loot_template 
+WHERE item IN (SELECT entry FROM item_template WHERE (class = 0 AND subclass = 5));
+
+-- Remove Food from Item Loot Table
+DELETE FROM item_loot_template 
+WHERE item IN (SELECT entry FROM item_template WHERE (class = 0 AND subclass = 5));
 
 -- Remove Food from Reference Loot Table
 DELETE FROM reference_loot_template 
 WHERE item IN (SELECT entry FROM item_template WHERE (class = 0 AND subclass = 5));
 
+-- GREYS
+
+-- Remove Greys from Creature Loot Table
+DELETE FROM creature_loot_template 
+WHERE item IN (SELECT entry FROM item_template WHERE quality = 0);
+
+-- Remove Greys from Game Object Loot Table
+DELETE FROM gameobject_loot_template 
+WHERE item IN (SELECT entry FROM item_template WHERE quality = 0);
+
+-- Remove Greys from Item Loot Table
+DELETE FROM item_loot_template 
+WHERE item IN (SELECT entry FROM item_template WHERE quality = 0);
+
+-- Remove Greys from Reference Loot Table
+DELETE FROM reference_loot_template 
+WHERE item IN (SELECT entry FROM item_template WHERE quality = 0);
+
+-- EMPTY REFERENCES
+
 -- Delete empty Reference loot tables from creature loot tables
 DELETE FROM creature_loot_template 
+WHERE Reference NOT IN (SELECT entry FROM reference_loot_template) AND Reference != 0;
+
+-- Delete empty Reference loot tables from gameobject loot tables
+DELETE FROM gameobject_loot_template 
+WHERE Reference NOT IN (SELECT entry FROM reference_loot_template) AND Reference != 0;
+
+-- Delete empty Reference loot tables from item_loot_template loot tables
+DELETE FROM item_loot_template 
 WHERE Reference NOT IN (SELECT entry FROM reference_loot_template) AND Reference != 0;
