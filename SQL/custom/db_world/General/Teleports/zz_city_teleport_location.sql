92f904f6a74cc0a06fbf9641106abffa84f71a95
justurn <justurn@live.com>
2024-01-06 12:18:32 +0800

    darnassus raid teleporter

--- SQL/custom/db_world/General/Teleports/zz_city_teleport_location.sql
+++ SQL/custom/db_world/General/Teleports/zz_city_teleport_location.sql
@@ -1,3 +1,4 @@
 REPLACE INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `Orientation`, `VerifiedBuild`) VALUES
 (3561, 0, 0, -8793.13, 651.03, 95.38, 3.87, 0), -- Stormwind
+(3565, 0, 1, 9867.54, 2494.37, 1316.28, 6.00, 0), -- Darnassus
 (3562, 0, 0, -4864.64, -931.35, 501.84, 3.75, 0); -- Ironforge
