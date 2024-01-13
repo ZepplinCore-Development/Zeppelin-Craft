cd59a2ec74b9847ffaea4b927112818489a0d987
justurn <justurn@live.com>
2024-01-13 12:59:41 +0800

    moved baelogs chest

---
+++ SQL/custom/db_world/Zones/Eastern Kingdoms/Badlands/Uldaman/zz_baelog_chest.sql
@@ -0,0 +1,2 @@
+REPLACE INTO acore_world.gameobject (guid,id,`map`,zoneId,areaId,spawnMask,phaseMask,position_x,position_y,position_z,orientation,rotation0,rotation1,rotation2,rotation3,spawntimesecs,animprogress,state,ScriptName,VerifiedBuild,Comment) VALUES
+(40673,123329,70,0,0,1,1,-37.8287,221.357,-48.3416,3.18789,-0.0,-0.0,-0.999732,0.0231445,300,0,1,'',NULL,NULL);
