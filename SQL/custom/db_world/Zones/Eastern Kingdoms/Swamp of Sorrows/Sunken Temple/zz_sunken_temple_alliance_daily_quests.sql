9d3f08a23fb5f9132b64d1f2ca701bcac453c911
justurn <justurn@live.com>
2024-01-27 18:34:22 +0800

    Lower Sunken Temple

--- SQL/custom/db_world/Zones/Eastern Kingdoms/Swamp of Sorrows/Sunken Temple/zz_sunken_temple_alliance_daily_quests.sql
+++ SQL/custom/db_world/Zones/Eastern Kingdoms/Swamp of Sorrows/Sunken Temple/zz_sunken_temple_alliance_daily_quests.sql
@@ -72,3 +72,79 @@ DELETE FROM `creature_questender` WHERE (`quest` = 441196);
 INSERT INTO `creature_questender` 
 SET `id` = 29093,
     `quest` = 441196;
+
+
+-- Atal'alarion --
+
+-- Atal'alarion - Pool Quest
+DELETE FROM `pool_quest` WHERE (`entry` = 441215);
+INSERT INTO `pool_quest` 
+SET `entry` = 441215,
+    `pool_entry` = 300010, -- Update for new daily
+    `description` = 'Atal''alarion';
+
+-- Atal'alarion - Quest Template
+DELETE FROM `quest_template` WHERE (`ID` = 441215);
+INSERT INTO `quest_template` 
+SET `ID` = 441215,
+    `QuestType` = 2,
+    `QuestLevel` = 55, -- Update for new daily
+    `MinLevel` = 51, -- Update for new daily
+    `QuestSortID` = 1417, -- Update for new daily
+    `QuestInfoID` = 81,
+    `RewardXPDifficulty` = 7,
+    `RewardMoney` = 132000,
+    `RewardBonusMoney` = 11400,
+    `StartItem` = 900264, -- Teleport Orb Update for new daily
+    `Flags` = 4104,
+    `RewardChoiceItemID1` = 900104, -- Emblem of Adventure
+    `RewardChoiceItemQuantity1` = 1,
+    `RewardChoiceItemID2` = 900189, -- Orb of Experience
+    `RewardChoiceItemQuantity2` = 1,
+    `RewardFactionID1` = 469, -- Alliance
+    `RewardFactionValue1` = 6, 
+    `LogTitle` = 'Wanted: Shirvallah''s Tear',
+    `LogDescription` = 'Retrieve the Shirvallah''s Tear from Atal''alarion in Temple of Atal''Hakkar.',
+    `QuestDescription` = 'Howdy, partner! I heard that them Horde Zugs, led by Zuglord Runthak, are schemin'' to snatch Shirvallah''s Tear from the likes of Atal''alarion down in The Temple of Atal''Hakkar. Can ya believe it? They''re talkin'' ''bout drinkin'' that sacred tear! We can''t let that happen! Saddle up, face Atal''alarion, and bring back Shirvallah''s Tear. Oh, and afore ya head out, we swiped an orb from them lazy book-readin'' mages over in the tower, it''ll teleport ya straight to the temple. Let''s put a stop to them Horde''s drinkin'' plans and keep Shirvallah''s Tear safe! We''ll make sure you''re well rewarded.',
+    `AreaDescription` = '',
+    `QuestCompletionLog` = 'Return the Shirvallah''s Tear to Travis Coomingham in Stormwind.',
+    `RequiredItemId1` = 900248, -- Update for new daily
+    `RequiredItemCount1` = 1,
+    `VerifiedBuild` = 12340;
+
+-- Atal'alarion - Quest Template Addon
+DELETE FROM `quest_template_addon` WHERE (`ID` = 441215);
+INSERT INTO `quest_template_addon` 
+SET `ID` = 441215,
+    `MaxLevel` = 58, -- Update for new daily
+    `ExclusiveGroup` = 441215,
+    `SpecialFlags` = 1;
+
+-- Atal'alarion - Quest Offer Reward
+DELETE FROM `quest_offer_reward` WHERE (`ID` = 441215);
+INSERT INTO `quest_offer_reward` 
+SET `ID` = 441215,
+    `Emote1` = 1,
+    `RewardText` = 'Thanks for thwartin'' them Horde plans, Zuglord Runthak''ll be seethin'' knowing we beat ''em to the punch.',
+    `VerifiedBuild` = 12340;
+
+-- Atal'alarion - Quest Request Items
+DELETE FROM `quest_request_items` WHERE (`ID` = 441215);
+INSERT INTO `quest_request_items` 
+SET `ID` = 441215,
+    `EmoteOnComplete` = 1,
+    `EmoteOnIncomplete` = 1,
+    `CompletionText` = 'The Shirvallah''s Tear please.',
+    `VerifiedBuild` = 12340;
+
+-- Atal'alarion - Creature Quest Starter
+DELETE FROM `creature_queststarter` WHERE (`quest` = 441215);
+INSERT INTO `creature_queststarter` 
+SET `id` = 29093,
+    `quest` = 441215;
+
+-- Atal'alarion - Creature Quest Ender
+DELETE FROM `creature_questender` WHERE (`quest` = 441215);
+INSERT INTO `creature_questender` 
+SET `id` = 29093,
+    `quest` = 441215;
--- SQL/custom/db_world/Zones/Eastern Kingdoms/Swamp of Sorrows/Sunken Temple/zz_sunken_temple_horde_daily_quests.sql
+++ SQL/custom/db_world/Zones/Eastern Kingdoms/Swamp of Sorrows/Sunken Temple/zz_sunken_temple_horde_daily_quests.sql
@@ -72,3 +72,79 @@ DELETE FROM `creature_questender` WHERE (`quest` = 441168);
 INSERT INTO `creature_questender` 
 SET `id` = 14392,
     `quest` = 441168;
+
+
+-- Atal'alarion --
+
+-- Atal'alarion - Pool Quest
+DELETE FROM `pool_quest` WHERE (`entry` = 441216);
+INSERT INTO `pool_quest` 
+SET `entry` = 441216,
+    `pool_entry` = 300004, -- Update for new daily
+    `description` = 'Atal''alarion';
+
+-- Atal'alarion - Quest Template
+DELETE FROM `quest_template` WHERE (`ID` = 441216);
+INSERT INTO `quest_template` 
+SET `ID` = 441216,
+    `QuestType` = 2,
+    `QuestLevel` = 55, -- Update for new daily
+    `MinLevel` = 51, -- Update for new daily
+    `QuestSortID` = 1417, -- Update for new daily
+    `QuestInfoID` = 81,
+    `RewardXPDifficulty` = 7,
+    `RewardMoney` = 132000,
+    `RewardBonusMoney` = 11400,
+    `StartItem` = 900264, -- Teleport Orb Update for new daily
+    `Flags` = 4104,
+    `RewardChoiceItemID1` = 900104, -- Emblem of Adventure
+    `RewardChoiceItemQuantity1` = 1,
+    `RewardChoiceItemID2` = 900189, -- Orb of Experience
+    `RewardChoiceItemQuantity2` = 1,
+    `RewardFactionID1` = 1801, -- Horde
+    `RewardFactionValue1` = 6, 
+    `LogTitle` = 'Wanted: Shirvallah''s Tear',
+    `LogDescription` = 'Retrieve the Shirvallah''s Tear from the Atal''alarion in the Temple of Atal''Hakkar.',
+    `QuestDescription` = 'Valiant champion, the call of the Horde echoes in the ancient halls of the Temple of Atal''Hakkar. Your mission is to defeat Atal''alarion, a formidable adversary, and recover Shirvallah''s Tear, a rare relic linked to the uncorrupted essence of Shirvallah. The Darkspear tribe holds a sacred purpose for this tear, to restore Shirvallah and cleanse the corruption that has befallen them. Venture forth, face the challenges within the temple, and emerge victorious against Atal''alarion. Bring back Shirvallah''s Tear and be the harbinger of restoration for the Horde. As you embark on this quest, utilize the enchanted orb provided to teleport to the Temple of Atal''Hakkar from any corner of Azeroth. May your strength prevail, champion of the Horde!',
+    `AreaDescription` = '',
+    `QuestCompletionLog` = 'Return the Shirvallah''s Tear to Overlord Runthak in Orgrimmar.',
+    `RequiredItemId1` = 900248, -- Update for new daily
+    `RequiredItemCount1` = 1,
+    `VerifiedBuild` = 12340;
+
+-- Atal'alarion - Quest Template Addon
+DELETE FROM `quest_template_addon` WHERE (`ID` = 441216);
+INSERT INTO `quest_template_addon` 
+SET `ID` = 441216,
+    `MaxLevel` = 58, -- Update for new daily
+    `ExclusiveGroup` = 441216,
+    `SpecialFlags` = 1;
+
+-- Atal'alarion - Quest Offer Reward
+DELETE FROM `quest_offer_reward` WHERE (`ID` = 441216);
+INSERT INTO `quest_offer_reward` 
+SET `ID` = 441216,
+    `Emote1` = 1,
+    `RewardText` = 'Your triumph over the Atal''alarion proves your worth. The Horde recognizes a true champion in your deeds.',
+    `VerifiedBuild` = 12340;
+
+-- Atal'alarion - Quest Request Items
+DELETE FROM `quest_request_items` WHERE (`ID` = 441216);
+INSERT INTO `quest_request_items` 
+SET `ID` = 441216,
+    `EmoteOnComplete` = 1,
+    `EmoteOnIncomplete` = 1,
+    `CompletionText` = 'The Shirvallah''s Tear please.',
+    `VerifiedBuild` = 12340;
+
+-- Atal'alarion - Creature Quest Starter
+DELETE FROM `creature_queststarter` WHERE (`quest` = 441216);
+INSERT INTO `creature_queststarter` 
+SET `id` = 14392,
+    `quest` = 441216;
+
+-- Atal'alarion - Creature Quest Ender
+DELETE FROM `creature_questender` WHERE (`quest` = 441216);
+INSERT INTO `creature_questender` 
+SET `id` = 14392,
+    `quest` = 441216;
