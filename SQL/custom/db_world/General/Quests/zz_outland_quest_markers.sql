cf87999e038619c0c974725e6647dbd11af3d20a
justurn <justurn@live.com>
2024-04-05 07:23:11 +0800

    Update zz_outland_quest_markers.sql

--- SQL/custom/db_world/General/Quests/zz_outland_quest_markers.sql
+++ SQL/custom/db_world/General/Quests/zz_outland_quest_markers.sql
@@ -1,3 +1,4 @@
+-- Kalimdor Zones
 UPDATE quest_poi
 SET MapID = 1
 WHERE (WorldMapAreaId = 476 OR WorldMapAreaId = 464);
@@ -13,3 +14,20 @@ WHERE QuestID IN (
       AND MapID = 1
 )
 AND VerifiedBuild = 0;
+
+-- Easern Kingdom Zones
+UPDATE quest_poi
+SET MapID = 0
+WHERE WorldMapAreaId IN (462, 463, 480);
+
+UPDATE quest_poi_points
+SET x = x - 2133,
+    y = y + 2135
+WHERE QuestID IN (
+    SELECT QuestID
+    FROM quest_poi
+    WHERE WorldMapAreaId IN (462, 463, 480)
+      AND MapID = 0
+);
+
+
