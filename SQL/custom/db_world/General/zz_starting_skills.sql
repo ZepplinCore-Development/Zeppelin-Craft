b19d8790d89bd97cc3c3d0db3ed6ca955de3e961
justurn <justurn@live.com>
2024-04-21 15:50:38 +0800

    Update zz_starting_skills.sql

--- SQL/custom/db_world/General/zz_starting_skills.sql
+++ SQL/custom/db_world/General/zz_starting_skills.sql
@@ -1,3 +1,3 @@
-REPLACE INTO playercreateinfo_skills (raceMask,classMask,skill,rank,comment) 
+REPLACE INTO playercreateinfo_skills (raceMask,classMask,skill,`rank`,comment) 
 	VALUES
 	(0,195,433,0,'Shield'); -- Mages now get shield, original value was 95
