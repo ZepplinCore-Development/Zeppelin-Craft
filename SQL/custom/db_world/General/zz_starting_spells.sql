4bef9d07e5b5753391a796e86683050a512ec43b
justurn <justurn@live.com>
2024-04-25 11:05:39 +0800

    Update zz_starting_spells.sql

--- SQL/custom/db_world/General/zz_starting_spells.sql
+++ SQL/custom/db_world/General/zz_starting_spells.sql
@@ -14,5 +14,6 @@ REPLACE INTO playercreateinfo_spell_custom (racemask,classmask,Spell,Note)
     (0,0,514,'Counterspell'),
     (0,0,8690,'Hearthstone'),
     (0,0,91049,'Find Minerals and Herbs'),
+    (0,3,91056,'Magical Attunement'),
     (0,1,91001,'Counter'),
     (0,2,91002,'Chastise');
