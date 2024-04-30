a9de55d01f305a6394d6ec1daa221435f0d8e60a
justurn <justurn@live.com>
2024-05-01 07:04:44 +0800

    Delete zz_deviate_shambler.sql

--- SQL/custom/db_world/Zones/Kalimdor/The Barrens/Wailing Caverns/zz_deviate_shambler.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM `creature_template_resistance` WHERE (`CreatureID` = 5761);
-INSERT INTO `creature_template_resistance` (`CreatureID`, `School`, `Resistance`, `VerifiedBuild`) VALUES
-(5761, 2, 50, 12340),
-(5761, 3, 50, 12340),
-(5761, 4, 50, 12340),
-(5761, 5, 50, 12340),
-(5761, 6, 50, 12340);
