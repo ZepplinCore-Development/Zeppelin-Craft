ea54ee13ad0e3a0c4c6328632288ece71056f73d
justurn <justurn@live.com>
2024-05-07 19:10:28 +0800

    Update zz_daily_dungeon_quest_pools.sql

--- SQL/custom/db_world/General/Dungeons/zz_daily_dungeon_quest_pools.sql
+++ SQL/custom/db_world/General/Dungeons/zz_daily_dungeon_quest_pools.sql
@@ -1,17 +1,23 @@
 -- VANILLA HORDE NORMAL DUNGEON POOLS
 REPLACE INTO `pool_template` (entry,max_limit,description) VALUES
-(300000,1,'Vanilla Horde Normal Dungeons Levels 15 - 25'),
-(300001,1,'Vanilla Horde Normal Dungeons Levels 26 - 35'),
-(300002,1,'Vanilla Horde Normal Dungeons Levels 36 - 45'),
+(300014,1,'Vanilla Horde Normal Dungeons Levels 15 - 20'),
+(300000,1,'Vanilla Horde Normal Dungeons Levels 21 - 25'),
+(300012,1,'Vanilla Horde Normal Dungeons Levels 26 - 30'),
+(300001,1,'Vanilla Horde Normal Dungeons Levels 31 - 35'),
+(300016,1,'Vanilla Horde Normal Dungeons Levels 36 - 40'),
+(300002,1,'Vanilla Horde Normal Dungeons Levels 41 - 45'),
 (300003,1,'Vanilla Horde Normal Dungeons Levels 46 - 50'),
 (300004,1,'Vanilla Horde Normal Dungeons Levels 51 - 58'),
 (300005,1,'Vanilla Horde Normal Dungeons Levels 59 - 60');
 
 -- VANILLA ALLIANCE NORMAL DUNGEON POOLS
 REPLACE INTO `pool_template` (entry,max_limit,description) VALUES
-(300006,1,'Vanilla Alliance Normal Dungeons Levels 15 - 25'),
-(300007,1,'Vanilla Alliance Normal Dungeons Levels 26 - 35'),
-(300008,1,'Vanilla Alliance Normal Dungeons Levels 36 - 45'),
+(300015,1,'Vanilla Alliance Normal Dungeons Levels 15 - 20'),
+(300006,1,'Vanilla Alliance Normal Dungeons Levels 21 - 25'),
+(300013,1,'Vanilla Alliance Normal Dungeons Levels 26 - 30'),
+(300007,1,'Vanilla Alliance Normal Dungeons Levels 31 - 35'),
+(300008,1,'Vanilla Alliance Normal Dungeons Levels 36 - 40'),
+(300008,1,'Vanilla Alliance Normal Dungeons Levels 41 - 45'),
 (300009,1,'Vanilla Alliance Normal Dungeons Levels 46 - 50'),
 (300010,1,'Vanilla Alliance Normal Dungeons Levels 51 - 58'),
 (300011,1,'Vanilla Alliance Normal Dungeons Levels 59 - 60');
