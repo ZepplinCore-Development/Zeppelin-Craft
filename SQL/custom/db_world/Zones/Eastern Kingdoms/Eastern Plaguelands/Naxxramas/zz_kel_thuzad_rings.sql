6df8c01e93bdcc6ac8bc9b29a8f8ebc01b8797e2
justurn <justurn@live.com>
2024-05-19 13:46:26 +0800

    Create zz_kel_thuzad_rings.sql

---
+++ SQL/custom/db_world/Zones/Eastern Kingdoms/Eastern Plaguelands/Naxxramas/zz_kel_thuzad_rings.sql
@@ -0,0 +1,10 @@
+DELETE FROM `reference_loot_template` WHERE (`Entry` = 304710) AND (`Item` IN (23060, 23061, 23062, 23063, 23064, 23065, 23066, 23067));
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(304710, 23060, 0, 0, 0, 1, 2, 1, 1, NULL),
+(304710, 23061, 0, 0, 0, 1, 3, 1, 1, NULL),
+(304710, 23062, 0, 0, 0, 1, 4, 1, 1, NULL),
+(304710, 23063, 0, 0, 0, 1, 5, 1, 1, NULL),
+(304710, 23064, 0, 0, 0, 1, 6, 1, 1, NULL),
+(304710, 23065, 0, 0, 0, 1, 7, 1, 1, NULL),
+(304710, 23066, 0, 0, 0, 1, 8, 1, 1, NULL),
+(304710, 23067, 0, 0, 0, 1, 9, 1, 1, NULL);
