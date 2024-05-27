3858451a9065e32ed58b571594278e7499015974
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2024-05-27 17:24:00 +0800

    patched aldor and scryer behavior

---
+++ SQL/custom/db_world/General/zz_aldor_and_scryer_patch.sql
@@ -0,0 +1,7 @@
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10551);
+UPDATE `quest_template` SET `RewardFactionOverride1` = 0 WHERE (`ID` = 10551);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10552);
+UPDATE `quest_template` SET `RewardFactionOverride1` = 0 WHERE (`ID` = 10552);
+
+
+
