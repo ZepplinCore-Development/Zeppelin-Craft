161b659f495dc25140a2e0ed06d7c52705049c56
justurn <justurn@live.com>
2024-06-01 20:36:53 +0800

    Update zz_arcane_ward.sql

--- SQL/custom/db_world/General/Items/zz_arcane_ward.sql
+++ SQL/custom/db_world/General/Items/zz_arcane_ward.sql
@@ -1,20 +1,134 @@
-DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
-INSERT INTO `item_template` (`entry`, `class`, `subclass`, `SoundOverrideSubclass`, `name`, `displayid`, `Quality`, `Flags`, `FlagsExtra`, `BuyCount`, `BuyPrice`, `SellPrice`, `InventoryType`, `AllowableClass`, `AllowableRace`, `ItemLevel`, `RequiredLevel`, `RequiredSkill`, `RequiredSkillRank`, `requiredspell`, `requiredhonorrank`, `RequiredCityRank`, `RequiredReputationFaction`, `RequiredReputationRank`, `maxcount`, `stackable`, `ContainerSlots`, `StatsCount`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `ScalingStatDistribution`, `ScalingStatValue`, `dmg_min1`, `dmg_max1`, `dmg_type1`, `dmg_min2`, `dmg_max2`, `dmg_type2`, `armor`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `delay`, `ammo_type`, `RangedModRange`, `spellid_1`, `spelltrigger_1`, `spellcharges_1`, `spellppmRate_1`, `spellcooldown_1`, `spellcategory_1`, `spellcategorycooldown_1`, `spellid_2`, `spelltrigger_2`, `spellcharges_2`, `spellppmRate_2`, `spellcooldown_2`, `spellcategory_2`, `spellcategorycooldown_2`, `spellid_3`, `spelltrigger_3`, `spellcharges_3`, `spellppmRate_3`, `spellcooldown_3`, `spellcategory_3`, `spellcategorycooldown_3`, `spellid_4`, `spelltrigger_4`, `spellcharges_4`, `spellppmRate_4`, `spellcooldown_4`, `spellcategory_4`, `spellcategorycooldown_4`, `spellid_5`, `spelltrigger_5`, `spellcharges_5`, `spellppmRate_5`, `spellcooldown_5`, `spellcategory_5`, `spellcategorycooldown_5`, `bonding`, `description`, `PageText`, `LanguageID`, `PageMaterial`, `startquest`, `lockid`, `Material`, `sheath`, `RandomProperty`, `RandomSuffix`, `block`, `itemset`, `MaxDurability`, `area`, `Map`, `BagFamily`, `TotemCategory`, `socketColor_1`, `socketContent_1`, `socketColor_2`, `socketContent_2`, `socketColor_3`, `socketContent_3`, `socketBonus`, `GemProperties`, `RequiredDisenchantSkill`, `ArmorDamageModifier`, `duration`, `ItemLimitCategory`, `HolidayId`, `ScriptName`, `DisenchantID`, `FoodType`, `minMoneyLoot`, `maxMoneyLoot`, `flagsCustom`, `VerifiedBuild`) VALUES
-(900285, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340),
-(900286, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340),
-(900287, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340),
-(900288, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340),
-(900289, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340),
-(900290, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340),
-(900291, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340),
-(900292, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340),
-(900293, 4, 0, -1, 'Arcane Ward', 54063, 5, 0, 0, 1, 0, 0, 23, 128, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91054, 1, 0, 0, -1, 0, -1, 0, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 12340);
+-- Arcane Ward 1
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902100,
+        `name` = 'Arcane Ward I',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 1,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1;    
 
-UPDATE `item_template` SET `ItemLevel` = 10 WHERE (`entry` = 900286);
-UPDATE `item_template` SET `ItemLevel` = 20 WHERE (`entry` = 900287);
-UPDATE `item_template` SET `ItemLevel` = 30 WHERE (`entry` = 900288);
-UPDATE `item_template` SET `ItemLevel` = 40 WHERE (`entry` = 900289);
-UPDATE `item_template` SET `ItemLevel` = 50 WHERE (`entry` = 900290);
-UPDATE `item_template` SET `ItemLevel` = 60 WHERE (`entry` = 900291);
-UPDATE `item_template` SET `ItemLevel` = 70 WHERE (`entry` = 900292);
-UPDATE `item_template` SET `ItemLevel` = 80 WHERE (`entry` = 900293);
+-- Arcane Ward 2
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902101,
+        `name` = 'Arcane Ward II',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 10,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1;    
+
+-- Arcane Ward 3
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902102,
+        `name` = 'Arcane Ward III',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 20,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1; 
+
+-- Arcane Ward 4
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902103,
+        `name` = 'Arcane Ward IV',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 30,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1; 
+
+-- Arcane Ward 5
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902104,
+        `name` = 'Arcane Ward V',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 40,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1;
+
+-- Arcane Ward 6
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902105,
+        `name` = 'Arcane Ward VI',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 50,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1;
+
+-- Arcane Ward 7
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902106,
+        `name` = 'Arcane Ward VII',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 60,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1;
+
+-- Arcane Ward 8
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902107,
+        `name` = 'Arcane Ward VIII',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 70,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1;
+
+-- Arcane Ward 9
+    DELETE FROM `item_template` WHERE (`name` = 'Arcane Ward');
+    INSERT INTO `item_template`
+    SET `entry` = 902108,
+        `name` = 'Arcane Ward IX',
+        `displayid` = 54063,
+        `Quality` = 5,
+        `bonding` = 1,
+        `ItemLevel` = 80,
+        `InventoryType` = 23,
+        `maxcount` = 1,
+        `AllowableClass` = 128,
+        `spellid_1` = 91054,
+        `spelltrigger_1` = 1;
