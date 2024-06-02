1f583120cf584b9a3f9596ba3c8ea0676a399a60
justurn <justurn@live.com>
2024-06-02 13:38:59 +0800

    Clean Mystic Resonator

--- SQL/custom/db_world/Professions/Enchanting/zz_mystic_resonator.sql
+++ SQL/custom/db_world/Professions/Enchanting/zz_mystic_resonator.sql
@@ -1,3 +1,35 @@
-DELETE FROM `item_template` WHERE (`entry` = 41605);
-INSERT INTO `item_template` (`entry`, `class`, `subclass`, `SoundOverrideSubclass`, `name`, `displayid`, `Quality`, `Flags`, `FlagsExtra`, `BuyCount`, `BuyPrice`, `SellPrice`, `InventoryType`, `AllowableClass`, `AllowableRace`, `ItemLevel`, `RequiredLevel`, `RequiredSkill`, `RequiredSkillRank`, `requiredspell`, `requiredhonorrank`, `RequiredCityRank`, `RequiredReputationFaction`, `RequiredReputationRank`, `maxcount`, `stackable`, `ContainerSlots`, `StatsCount`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `ScalingStatDistribution`, `ScalingStatValue`, `dmg_min1`, `dmg_max1`, `dmg_type1`, `dmg_min2`, `dmg_max2`, `dmg_type2`, `armor`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `delay`, `ammo_type`, `RangedModRange`, `spellid_1`, `spelltrigger_1`, `spellcharges_1`, `spellppmRate_1`, `spellcooldown_1`, `spellcategory_1`, `spellcategorycooldown_1`, `spellid_2`, `spelltrigger_2`, `spellcharges_2`, `spellppmRate_2`, `spellcooldown_2`, `spellcategory_2`, `spellcategorycooldown_2`, `spellid_3`, `spelltrigger_3`, `spellcharges_3`, `spellppmRate_3`, `spellcooldown_3`, `spellcategory_3`, `spellcategorycooldown_3`, `spellid_4`, `spelltrigger_4`, `spellcharges_4`, `spellppmRate_4`, `spellcooldown_4`, `spellcategory_4`, `spellcategorycooldown_4`, `spellid_5`, `spelltrigger_5`, `spellcharges_5`, `spellppmRate_5`, `spellcooldown_5`, `spellcategory_5`, `spellcategorycooldown_5`, `bonding`, `description`, `PageText`, `LanguageID`, `PageMaterial`, `startquest`, `lockid`, `Material`, `sheath`, `RandomProperty`, `RandomSuffix`, `block`, `itemset`, `MaxDurability`, `area`, `Map`, `BagFamily`, `TotemCategory`, `socketColor_1`, `socketContent_1`, `socketColor_2`, `socketContent_2`, `socketColor_3`, `socketContent_3`, `socketBonus`, `GemProperties`, `RequiredDisenchantSkill`, `ArmorDamageModifier`, `duration`, `ItemLimitCategory`, `HolidayId`, `ScriptName`, `DisenchantID`, `FoodType`, `minMoneyLoot`, `maxMoneyLoot`, `flagsCustom`, `VerifiedBuild`) VALUES
-(41605, 0, 6, -1, 'Mystic Resonator', 23286, 4, 32848, 8192, 1, 30000, 7500, 0, -1, -1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10694, 0, -1, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '|cff00ff00Use: Adds or re-rolls an extra stat on to an item.|n|n|cffff0000WARNING: This will replace any extra stats with a new one.', 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 15595);
+-- Formula: Mystic Resonator
+    DELETE FROM `item_template` WHERE (`entry` = 902312);
+    INSERT INTO `item_template`
+    SET `entry` = 902311,
+        `class` = 9, -- Recipe
+        `subclass` = 8, -- Enchanting
+        `name` = 'Formula: Mystic Resonator',
+        `displayid` = 1301,
+        `SellPrice` = 100000, -- 10G 
+        `Quality` = 4, -- Epic
+        `Flags` = 64, -- PLAYERCAST 
+        `bonding` = 1, -- BOP
+        `RequiredSkill` = 333, -- Enchanting
+        `RequiredSkillRank` = 300,
+        `spellid_1` = 483, -- Learn Spell
+        `spellcharges_1` = -1,
+        `spellid_2` = 72559,
+        `spelltrigger_2` = 6;
+
+-- Mystic Resonator
+    DELETE FROM `item_template` WHERE (`entry` = 902313);
+    INSERT INTO `item_template`
+    SET `entry` = 902312,
+        `class` = 0, -- Consumable
+        `subclass` = 6 -- Item Enhancement
+        `SellPrice` = 100000, -- 10G       
+        `name` = 'Mystic Resonator',
+        `description` = '|cff00ff00Use: Adds or re-rolls an extra stat on to an item.|n|n|cffff0000WARNING: This will replace any extra stats with a new one.',
+        `displayid` = 23286,
+        `stackable` = 20,
+        `Quality` = 4, -- Epic
+        `Flags` = 32832, -- PLAYERCAST + NO DISENCHANT
+        `bonding` = 1, -- BOE
+        `spellid_1` = 10694,
+        `spellcharges_1` = -1;
