72e0f9685b5bebfbb3b8336bd4dda1d30642d321
justurn <justurn@live.com>
2024-06-02 13:49:23 +0800

    Update zz_loot_King_Gordok.sql

--- SQL/custom/db_world/Zones/Kalimdor/Feralas/Dire Maul/zz_loot_King_Gordok.sql
+++ SQL/custom/db_world/Zones/Kalimdor/Feralas/Dire Maul/zz_loot_King_Gordok.sql
@@ -1,13 +1,9 @@
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 11501);
+-- King Gordok Loot
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 11501) AND (`Item` IN (902314));
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(11501, 14047, 0, 13, 0, 1, 0, 2, 5, 'King Gordok - Runecloth'),
-(11501, 18332, 0, 2, 0, 1, 1, 1, 1, 'King Gordok - Libram of Rapidity'),
-(11501, 18333, 0, 2, 0, 1, 1, 1, 1, 'King Gordok - Libram of Focus'),
-(11501, 18334, 0, 2, 0, 1, 1, 1, 1, 'King Gordok - Libram of Protection'),
-(11501, 18640, 0, 2, 0, 1, 0, 1, 1, 'King Gordok - Happy Fun Rock'),
-(11501, 18780, 0, 9, 0, 1, 0, 1, 1, 'King Gordok - Top Half of Advanced Armorsmithing: Volume I'),
-(11501, 19258, 0, 7, 0, 1, 0, 1, 1, 'King Gordok - Ace of Warlords'),
-(11501, 21982, 0, 80, 1, 1, 0, 1, 1, 'King Gordok - Ogre Warbeads'),
-(11501, 35016, 35016, 2, 0, 1, 1, 1, 1, 'King Gordok - (ReferenceTable)'),
-(11501, 35019, 35019, 100, 0, 1, 1, 1, 2, 'King Gordok - (ReferenceTable)'),
-(11501, 91000, 0, 4, 0, 1, 0, 1, 1, '');
+(11501, 902314, 0, 10, 0, 1, 1, 1, 1, 'Formula: Arcane Transmutation: Large Brilliant Shard to Nexus');
+
+-- Tribute Loot
+DELETE FROM `reference_loot_template` WHERE (`Entry` = 12007) AND (`Item` IN (902314));
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(12007, 902314, 0, 100, 0, 32, 2, 1, 1, 'Formula: Arcane Transmutation: Large Brilliant Shard to Nexus');
