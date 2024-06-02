1b4eb01f2bfa635a60ce51dc70dbb339effde3b7
justurn <justurn@live.com>
2024-06-02 20:47:08 +0800

    Update zz_harbinger_of_doom.sql

--- SQL/custom/db_world/General/Items/zz_harbinger_of_doom.sql
+++ SQL/custom/db_world/General/Items/zz_harbinger_of_doom.sql
@@ -1,1 +1,2 @@
+-- Harbinger of Doom - Replaced Hit rating with Attack Power and removed Unique-Equipped.
 UPDATE `item_template` SET `Flags` = 0, `stat_type4` = 38, `stat_value4` = 20 WHERE (`entry` = 23044);
