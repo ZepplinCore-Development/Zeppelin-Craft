2fa52efde26b8b9c5cfbfd53abdee5acb98595e3
justurn <justurn@live.com>
2024-06-02 11:44:14 +0800

    Cleaned Elementium Talisman

--- SQL/custom/db_world/Professions/Jewelcrafting/zz_elementium_talisman.sql
+++ SQL/custom/db_world/Professions/Jewelcrafting/zz_elementium_talisman.sql
@@ -1,3 +1,38 @@
-DELETE FROM `item_template` WHERE (`entry` = 1186);
-INSERT INTO `item_template` (`entry`, `class`, `subclass`, `SoundOverrideSubclass`, `name`, `displayid`, `Quality`, `Flags`, `FlagsExtra`, `BuyCount`, `BuyPrice`, `SellPrice`, `InventoryType`, `AllowableClass`, `AllowableRace`, `ItemLevel`, `RequiredLevel`, `RequiredSkill`, `RequiredSkillRank`, `requiredspell`, `requiredhonorrank`, `RequiredCityRank`, `RequiredReputationFaction`, `RequiredReputationRank`, `maxcount`, `stackable`, `ContainerSlots`, `StatsCount`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `ScalingStatDistribution`, `ScalingStatValue`, `dmg_min1`, `dmg_max1`, `dmg_type1`, `dmg_min2`, `dmg_max2`, `dmg_type2`, `armor`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `delay`, `ammo_type`, `RangedModRange`, `spellid_1`, `spelltrigger_1`, `spellcharges_1`, `spellppmRate_1`, `spellcooldown_1`, `spellcategory_1`, `spellcategorycooldown_1`, `spellid_2`, `spelltrigger_2`, `spellcharges_2`, `spellppmRate_2`, `spellcooldown_2`, `spellcategory_2`, `spellcategorycooldown_2`, `spellid_3`, `spelltrigger_3`, `spellcharges_3`, `spellppmRate_3`, `spellcooldown_3`, `spellcategory_3`, `spellcategorycooldown_3`, `spellid_4`, `spelltrigger_4`, `spellcharges_4`, `spellppmRate_4`, `spellcooldown_4`, `spellcategory_4`, `spellcategorycooldown_4`, `spellid_5`, `spelltrigger_5`, `spellcharges_5`, `spellppmRate_5`, `spellcooldown_5`, `spellcategory_5`, `spellcategorycooldown_5`, `bonding`, `description`, `PageText`, `LanguageID`, `PageMaterial`, `startquest`, `lockid`, `Material`, `sheath`, `RandomProperty`, `RandomSuffix`, `block`, `itemset`, `MaxDurability`, `area`, `Map`, `BagFamily`, `TotemCategory`, `socketColor_1`, `socketContent_1`, `socketColor_2`, `socketContent_2`, `socketColor_3`, `socketContent_3`, `socketBonus`, `GemProperties`, `RequiredDisenchantSkill`, `ArmorDamageModifier`, `duration`, `ItemLimitCategory`, `HolidayId`, `ScriptName`, `DisenchantID`, `FoodType`, `minMoneyLoot`, `maxMoneyLoot`, `flagsCustom`, `VerifiedBuild`) VALUES
-(1186, 4, 0, -1, 'Elementium Talisman', 35130, 4, 0, 0, 1, 10000000, 5000000, 12, -1, -1, 65, 60, 755, 300, 0, 0, 0, 0, 0, 1, 1, 0, 0, 32, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 72561, 1, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 1, '', 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 1);
+-- Design: Elementium Talisman
+    DELETE FROM `item_template` WHERE (`entry` = 902300);
+    INSERT INTO `item_template`
+    SET `entry` = 902300,
+        `class` = 9, -- Recipe
+        `name` = 'Design: Elementium Talisman',
+        `displayid` = 1102,
+        `SellPrice` = 1000000, -- 100G 
+        `Quality` = 4, -- Epic
+        `Flags` = 64, -- PLAYERCAST 
+        `bonding` = 1, -- BOP
+        `RequiredSkill` = 755, -- Jewelcrafting
+        `RequiredSkillRank` = 300,
+        `spellid_1` = 483, -- Learn Spell
+        `spellcharges_1` = -1,
+        `spellid_2` = 91076,
+        `spelltrigger_2` = 6;
+
+-- Elementium Talisman
+    DELETE FROM `item_template` WHERE (`entry` = 902301);
+    INSERT INTO `item_template`
+    SET `entry` = 902301,
+        `class` = 4, -- Armor
+        `InventoryType` = 12, -- Trinket
+        `SellPrice` = 1000000, -- 100G       
+        `maxcount` = 1, -- Unique       
+        `name` = 'Elementium Talisman',
+        `displayid` = 35130,
+        `Quality` = 4, -- Epic
+        `ItemLevel` = 65,
+        `RequiredLevel` = 60,
+        `bonding` = 1, -- BOP
+        `RequiredSkill` = 755, -- Jewelcrafting
+        `RequiredSkillRank` = 300,
+        `stat_type1` = 32, -- crit rating
+        `stat_value1` = 14,
+        `spellid_1` = 91077,
+        `spelltrigger_1` = 1;
