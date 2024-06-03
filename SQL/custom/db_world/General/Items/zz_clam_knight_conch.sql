420196c358c2ea84b9ac9d5d7e343755f2c2df13
justurn <justurn@live.com>
2024-06-03 10:35:19 +0800

    Update zz_clam_knight_conch.sql

--- SQL/custom/db_world/General/Items/zz_clam_knight_conch.sql
+++ SQL/custom/db_world/General/Items/zz_clam_knight_conch.sql
@@ -4,8 +4,8 @@ INSERT INTO `item_template` (`entry`, `class`, `subclass`, `SoundOverrideSubclas
 (902329, 15, 0, -1, 'Clam Knight Conch', 7177, 4, 4, 0, 1, 0, 0, 0, -1, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 'Caress the conch to claim the Clam Knight crafting cache', 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 1);
 
 -- Item loot
-DELETE FROM `item_loot_template` WHERE (`Entry` = 16068) AND (`Item` IN (32566, 33223, 54452));
+DELETE FROM `item_loot_template` WHERE (`Entry` = 902329) AND (`Item` IN (32566, 33223, 54452));
 INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(16068, 902310, 0, 100, 0, 1, 4, 1, 1, 'Compressed Thermal Anvil'),
-(16068, 902327, 0, 100, 0, 1, 5, 1, 1, 'Portable Guild Bank'),
-(16068, 902328, 0, 100, 0, 1, 2, 1, 1, 'Reagent Portal');
+(902329, 902310, 0, 100, 0, 1, 4, 1, 1, 'Compressed Thermal Anvil'),
+(902329, 902327, 0, 100, 0, 1, 5, 1, 1, 'Portable Guild Bank'),
+(902329, 902328, 0, 100, 0, 1, 2, 1, 1, 'Reagent Portal');
