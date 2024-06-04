1aeb05cdac544dc952c649bb89c27811ea43c0b5
justurn <justurn@live.com>
2024-06-04 20:56:33 +0800

    Fel Iron Arrows

---
+++ SQL/custom/db_world/Professions/Engineering/Items/zz_fel_iron_arrows.sql
@@ -0,0 +1,37 @@
+-- Schematic: Fel Iron Arrows
+    DELETE FROM `item_template` WHERE (`entry` = 902331);
+    INSERT INTO `item_template`
+    SET `entry` = 902331,
+        `class` = 9, -- Recipe
+        `subclass` = 4, -- Engineering
+        `name` = 'Schematic: Fel Iron Arrows',
+        `displayid` = 6270,
+        `SellPrice` = 100000, -- 10G 
+        `Quality` = 3, -- UNCOMMON
+        `Flags` = 64, -- PLAYERCAST 
+        `bonding` = 1, -- BOP
+        `RequiredSkill` = 202, -- Engineering
+        `RequiredSkillRank` = 300,
+        `spellid_1` = 483, -- Learn Spell
+        `spellcharges_1` = -1,
+        `spellid_2` = 91087,
+        `spelltrigger_2` = 6;
+
+-- Fel Iron Arrows
+    DELETE FROM `item_template` WHERE (`entry` = 902332);
+    INSERT INTO `item_template`
+    SET `entry` = 902332,
+        `class` = 6, -- Projectile
+        `subclass` = 2, -- Arrow
+        `name` = 'Fel Iron Arrows',
+        `displayid` = 40521,
+        `SellPrice` = 2, -- 2C
+        `Quality` = 2, -- UNCOMMON
+        `InventoryType` = 24, -- Ammo
+        `bonding` = 1, -- BOP
+        `BagFamily` = 1, -- Arrow
+        `ItemLevel` = 96,
+        `RequiredLevel` = 58,
+        `dmg_min1` = 26,
+        `dmg_max1` = 26,
+        `delay` = 3000;
