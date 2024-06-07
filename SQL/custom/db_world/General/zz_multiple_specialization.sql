4ce217af59ace25bff9b08cd9e3fd42a5aea5afc
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2024-06-07 11:53:03 +0800

    allowed multiple tailoring and alchemy specializations

---
+++ SQL/custom/db_world/General/zz_multiple_specialization.sql
@@ -0,0 +1,9 @@
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10831);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10832);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10833);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10907);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10906);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10905);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10902);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10899);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10897);
