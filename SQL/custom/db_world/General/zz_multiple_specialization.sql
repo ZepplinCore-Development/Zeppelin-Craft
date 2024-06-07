3759cc2e6ada68442a9d3d0abe2dd822a191cd24
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2024-06-07 12:12:44 +0800

    allowed multispec armor and weaponsmithing.

--- SQL/custom/db_world/General/zz_multiple_specialization.sql
+++ SQL/custom/db_world/General/zz_multiple_specialization.sql
@@ -1,9 +1,17 @@
+-- Tailoring
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10831);
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10832);
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10833);
+-- Alchemy
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10907);
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10906);
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10905);
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10902);
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10899);
 UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 10897);
+-- Blacksmithing Armor smithing
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 5283);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 5301);
+-- Blacksmithing Weapon smithing
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 5284);
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE (`ID` = 5302);
