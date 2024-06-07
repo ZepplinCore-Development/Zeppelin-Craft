505412dfa9a291543b0015b133469abb22032736
justurn <justurn@live.com>
2024-06-07 19:52:59 +0800

    icon tweaks

--- Scripts/Extra Icons/Extra Icons.py
+++ Scripts/Extra Icons/Extra Icons.py
@@ -23,20 +23,14 @@ blp_files = [line.strip() for line in lines]
 # Starting ID
 starting_id = 134241
 
-# Prepare the SQL query template for individual values
-value_template = "({}, '{}')"
+# Prepare the SQL query template
+sql_template = "REPLACE INTO itemdisplayinfo (ID, InventoryIcon1) VALUES ({}, '{}');"
 
-# Prepare a list to collect groups of values
-grouped_queries = []
+# Generate the SQL queries
+sql_queries = [sql_template.format(starting_id + i, blp_files[i]) for i in range(len(blp_files))]
 
-# Collect the values in groups of 1
-for i in range(0, len(blp_files), 1):
-    group = blp_files[i:i + 1]
-    values = ", ".join([value_template.format(starting_id + j, group[j]) for j in range(len(group))])
-    grouped_queries.append(f"INSERT INTO itemdisplayinfo (ID, InventoryIcon1) VALUES {values};")
-
-# Join the grouped queries into a single string with new lines
-final_sql_query = "\n".join(grouped_queries)
+# Join the queries into a single string with new lines
+final_sql_query = "\n".join(sql_queries)
 
 # Output the final SQL query
 print(f"SAVED QUERY")
