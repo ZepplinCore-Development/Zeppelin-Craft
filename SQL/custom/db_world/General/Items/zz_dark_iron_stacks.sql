5a094eb9ee4d0e3ef03618cb9b69879ae79e5a3b
justurn <justurn@live.com>
2024-06-09 12:36:43 +0800

    Create zz_dark_iron_stacks.sql

---
+++ SQL/custom/db_world/General/Items/zz_dark_iron_stacks.sql
@@ -0,0 +1,5 @@
+-- Dark Iron Residue stacks to 1000
+UPDATE `item_template` SET `stackable` = 1000 WHERE (`entry` = 18945);
+
+-- Dark Iron Scraps stacks to 2000
+UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 22528);
