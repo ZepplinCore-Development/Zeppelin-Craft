cc1426cedd9cdcd21dabf8bf2cbfae3185458c81
justurn <justurn@live.com>
2024-06-13 20:14:30 +0800

    exarch maladaar nerf

---
+++ SQL/custom/db_world/Zones/Outland/Terrokar Forrest/Auchindoun/Shadow Labs/zz_void_traveler_nerf.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `HealthModifier` = 0.25 WHERE (`entry` = 19226); -- normal
+UPDATE `creature_template` SET `HealthModifier` = 0.31 WHERE (`entry` = 20664); -- heroic
--- SQL/custom/db_world/Zones/Outland/Terrokar Forrest/Shattrath/Auchindoun/Shadow Labs/zz_void_traveler_nerf.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `creature_template` SET `HealthModifier` = 0.25 WHERE (`entry` = 19226); -- normal
-UPDATE `creature_template` SET `HealthModifier` = 0.31 WHERE (`entry` = 20664); -- heroic
