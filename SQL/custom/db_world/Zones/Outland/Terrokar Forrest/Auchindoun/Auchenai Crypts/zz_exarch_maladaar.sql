7ca270125dfbe8c5460bdb8457bcf34866fb68e6
justurn <justurn@live.com>
2024-06-16 13:13:48 +0800

    Update zz_exarch_maladaar.sql

--- SQL/custom/db_world/Zones/Outland/Terrokar Forrest/Auchindoun/Auchenai Crypts/zz_exarch_maladaar.sql
+++ SQL/custom/db_world/Zones/Outland/Terrokar Forrest/Auchindoun/Auchenai Crypts/zz_exarch_maladaar.sql
@@ -1,2 +1,4 @@
 -- reduced damage modifier from 14 as this is much higher than any other normal boss, and even higher than heroic maladaar...
-UPDATE `creature_template` SET `DamageModifier` = 9 WHERE (`entry` = 18373);
+UPDATE `creature_template` SET `DamageModifier` = 6 WHERE (`entry` = 18373);
+-- reduce attack speed from 1391ms to 2000ms on the Avatar of the martyred. too much damage coming through.
+UPDATE `creature_template` SET `BaseAttackTime` = 2000 WHERE (`entry` = 18478);
