3dea2da52dd207125e1057f6cb1918095709f5e9
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2024-06-18 14:52:49 +0800

    fixxed goblin rep fix quests

---
+++ SQL/custom/db_world/General/Quests/zz_goblin_rep_fix.sql
@@ -0,0 +1,3 @@
+UPDATE `quest_template` SET `RewardFactionOverride1` = 0, `RewardFactionOverride2` = 0 WHERE (`ID` = 9266);
+UPDATE `quest_template` SET `RewardFactionOverride1` = 0, `RewardFactionOverride2` = 0 WHERE (`ID` = 9268);
+UPDATE `quest_template` SET `RewardFactionOverride1` = 0, `RewardFactionOverride2` = 0 WHERE (`ID` = 9267);
