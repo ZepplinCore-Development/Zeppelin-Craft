eaee91940c2c5cf6ea5f2980d5bbf755fbb7d469
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2024-06-20 19:09:25 +0800

    added nightmare seeds to drop table

---
+++ SQL/custom/db_world/Professions/Herbalism/zz_nightmare_seeds.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject_loot_template` WHERE (`Entry` = 18118) AND (`Item` IN (22797));
+INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(18118, 22797, 0, 10, 0, 1, 0, 1, 1, '');
