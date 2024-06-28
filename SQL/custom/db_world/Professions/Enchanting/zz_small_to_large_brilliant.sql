d235902c07c8dbe855333b09a5a8256b1d6f80e4
justurn <justurn@live.com>
2024-06-28 19:06:08 +0800

    Update zz_small_to_large_brilliant.sql

--- SQL/custom/db_world/Professions/Enchanting/zz_small_to_large_brilliant.sql
+++ SQL/custom/db_world/Professions/Enchanting/zz_small_to_large_brilliant.sql
@@ -1,5 +1,5 @@
 -- Add to Expert enchanting trainers
-DELETE FROM `npc_trainer` WHERE `ID` = 201009 AND `SpellID` = 91117
+DELETE FROM `npc_trainer` WHERE (`ID` = 201009 AND `SpellID` = 91117);
 INSERT INTO `npc_trainer`
 SET `ID` = 201009,
     `SpellID` = 91117,
