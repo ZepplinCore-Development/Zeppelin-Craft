0b82fab426b18c36afcb6e85e6d81b35b3fa44fe
justurn <justurn@live.com>
2024-07-06 12:44:03 +0800

    Update zz_totem_category.sql

--- SQL/custom/db_world/Professions/Jewelcrafting/zz_totem_category.sql
+++ SQL/custom/db_world/Professions/Jewelcrafting/zz_totem_category.sql
@@ -2,6 +2,8 @@ SELECT *
 FROM dbc.spell
 WHERE Totem1 = 20815;
 
+-- Totem1 20824 is simple grinder used for cutting gems
+
 UPDATE dbc.spell
 SET Totem1 = 0, TotemCategory1 = 191
 WHERE Totem1 = 20815;
