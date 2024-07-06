019176e37aa25f89a452de6cb38912b77d6ef3d4
justurn <justurn@live.com>
2024-07-06 21:35:21 +0800

    engineering script tidy up

--- SQL/custom/db_world/Professions/Jewelcrafting/zz_Black_Diamond_Dragonling.sql
+++ SQL/custom/db_world/Professions/Jewelcrafting/zz_Black_Diamond_Dragonling.sql
@@ -38,3 +38,8 @@
         `stat_value1` = 48,
         `spellid_1` = 91008,
         `spellcooldown_1` = 300000;
+
+DELETE FROM `creature_template` WHERE (`entry` = 1979);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(1979, 0, 0, 0, 0, 0, 19295, 0, 0, 0, 'Black Diamond Dragonling', '', NULL, 0, 60, 60, 0, 35, 0, 1.11111, 0.857143, 1, 1, 18, 1, 0, 0, 3, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 1, '', 0, 1, 1, 1, 1.1, 0.5, 0, 100, 1, 8405008, 0, 0, 'npc_arcanite_dragonling', 12340);
+
