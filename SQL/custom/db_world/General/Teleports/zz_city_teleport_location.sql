6f82a8cab8273bca1149348367764d5955e282b7
justurn <justurn@live.com>
2024-07-14 20:06:59 +0800

    Update zz_city_teleport_location.sql

--- SQL/custom/db_world/General/Teleports/zz_city_teleport_location.sql
+++ SQL/custom/db_world/General/Teleports/zz_city_teleport_location.sql
@@ -2,4 +2,5 @@ REPLACE INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`,
 (3561, 0, 0, -8793.1, 651.0, 95.4, 3.9, 0), -- Stormwind
 (3565, 0, 1, 10038, 2479, 1318.8, 1.6, 0), -- Darnassus
 (3562, 0, 0, -4864.6, -931.4, 501.8, 3.8, 0), -- Ironforge
+(33690, 0, 0, -1801.1, 5157.3, -40.2, 5.5, 0), -- Shattrath
 (3567, 0, 0, 1601.6, -4316.4, 2.7, 4.9, 0); -- Orgrimmar
