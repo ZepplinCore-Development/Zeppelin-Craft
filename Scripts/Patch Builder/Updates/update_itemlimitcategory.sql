f570fe5577e292e8e0c2e4b11ea470f68b74e546
justurn <justurn@live.com>
2024-07-20 20:44:43 +0800

    Create update_itemlimitcategory.sql

---
+++ Scripts/Patch Builder/Updates/update_itemlimitcategory.sql
@@ -0,0 +1,14 @@
+DELETE FROM `dbc`.`itemlimitcategory` WHERE `ID` = 86;
+INSERT INTO `dbc`.`itemlimitcategory` SET 
+    `ID` = 86,
+    `name1` = 'Arcane Ward',
+    `nameFlags` = 16712190,
+    `quantity` = 1;
+
+DELETE FROM `dbc`.`itemlimitcategory` WHERE `ID` = 87;
+INSERT INTO `dbc`.`itemlimitcategory` SET 
+    `ID` = 87,
+    `name1` = 'Empowered Smithing Hammer',
+    `nameFlags` = 16712190,
+    `quantity` = 1;
+
