e08aa9020dd05aca03ae710d657ab94f032eec92
justurn <justurn@live.com>
2024-07-20 21:00:02 +0800

    Update zz_hammers.sql

--- SQL/custom/db_world/Professions/Blacksmithing/zz_hammers.sql
+++ SQL/custom/db_world/Professions/Blacksmithing/zz_hammers.sql
@@ -2,8 +2,9 @@
 UPDATE `item_template`
 SET `class` = 15, -- Miscellaneous
     `subclass` = 1, -- Reagent 
-    `name` = 'Smithing Hammer',
+    `name` = 'Basic Smithing Hammer',
     `InventoryType` = 0,
+    `Quality` = 6, -- Artifact
     `dmg_min1` = 0,
     `dmg_max1` = 0,
     `delay` = 0
@@ -20,7 +21,7 @@ SET `entry` = 57003,
     `ItemLevel` = 20,
     `name` = 'Bronze Smithing Hammer',
     `displayid` = 142061,
-    `Quality` = 1, -- Common
+    `Quality` = 6, -- Artifact
     `BagFamily` = 1152, -- Mining + Engineering
     `TotemCategory` = 162,
     `spellid_1` = 91120,
@@ -35,3 +36,30 @@ SET `ID` = 201004,
     `MoneyCost` = 500, -- 5S 
     `ReqSkillLine` = 164, -- Blacksmithing
     `ReqSkillRank` = 100;
+
+-- Iron Smithing Hammer ITEM TEMPLATE
+DELETE FROM `item_template` WHERE (`entry` = 57004);
+INSERT INTO `item_template`
+SET `entry` = 57004,
+    `class` = 15, -- Miscellaneous
+    `subclass` = 1, -- Reagent 
+    `SellPrice` = 30, -- 30C
+    `ItemLimitCategory` = 87,
+    `ItemLevel` = 20,
+    `name` = 'Iron Smithing Hammer',
+    `displayid` = 137284,
+    `Quality` = 6, -- Artifact
+    `BagFamily` = 1152, -- Mining + Engineering
+    `TotemCategory` = 162,
+    `spellid_1` = 91122,
+    `spelltrigger_1` = 5,
+    `bonding` = 1; -- BOP
+
+-- Iron Smithing Hammer TRAINER
+DELETE FROM `npc_trainer` WHERE (`ID` = 201004 AND `SpellID` = 91123);
+INSERT INTO `npc_trainer`
+SET `ID` = 201004,
+    `SpellID` = 91123,
+    `MoneyCost` = 500, -- 5S 
+    `ReqSkillLine` = 164, -- Blacksmithing
+    `ReqSkillRank` = 150;
