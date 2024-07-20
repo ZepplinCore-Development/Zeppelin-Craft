4a2689b57129fdbab390f38f52fd950fd1b1abda
justurn <justurn@live.com>
2024-07-20 21:45:38 +0800

    Create update_creaturefamily.sql

---
+++ Scripts/Patch Builder/Updates/update_creaturefamily.sql
@@ -0,0 +1,16 @@
+DELETE FROM `dbc`.`creaturefamily` WHERE `ID` = 47;
+INSERT INTO `dbc`.`creaturefamily` SET 
+    `ID` = 47,
+    `minScale` = 0.5,
+    `minScaleLevel` = 1,
+    `maxScale` = 0.6,
+    `maxScaleLevel` = 60,
+    `skillline_1` = 789,
+    `skillline_2` = 270,
+    `petfoodmask` = 1,
+    `pettalenttype` = 1,
+    `categoryenumid` = 64,
+    `name1` = 'Cloud Serpent',
+    `nameFlags` = 16712190,
+    `inconpath` = 'InterfaceIconsSpell_Nature_GuardianWard';
+
