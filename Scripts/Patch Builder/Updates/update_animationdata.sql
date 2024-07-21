274dadef4ae85d67d33fd70d975e19f6935f90da
justurn <justurn@live.com>
2024-07-21 20:10:49 +0800

    more patch diff

---
+++ Scripts/Patch Builder/Updates/update_animationdata.sql
@@ -0,0 +1,9 @@
+DELETE FROM `dbc`.`animationdata` WHERE `ID` = 646;
+INSERT INTO `dbc`.`animationdata` SET 
+    `ID` = 646,
+    `Name` = 'MountSelfStart',
+    `WeaponFlags` = 16,
+    `BodyFlags` = 256,
+    `Flags` = 3,
+    `BehaviourID` = 646;
+
