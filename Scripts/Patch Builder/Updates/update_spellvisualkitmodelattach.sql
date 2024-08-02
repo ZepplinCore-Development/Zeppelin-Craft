8252b3e50a19b0c7b6c9f09c0fa71d8720348690
justurn <justurn@live.com>
2024-08-02 13:00:41 +0800

    Create update_spellvisualkitmodelattach.sql

---
+++ Scripts/Patch Builder/Updates/update_spellvisualkitmodelattach.sql
@@ -0,0 +1,6 @@
+UPDATE `dbc`.`spellvisualkitmodelattach` SET 
+    `SpellVisualEffectNameId` = 4884 /* was 625 */,
+    `AttachmentId` = 17 /* was 21 */,
+    `Pitch` = 1.57 /* was 0.0 */
+WHERE `ID` = 1061;
+
