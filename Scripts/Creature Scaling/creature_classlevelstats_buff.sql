0f35e96b53b433f2928c1cfb8268cd2524275d7d
justurn <justurn@live.com>
2024-08-16 21:30:55 +0800

    Vanilla creature power reduction :arrow_down:

--- Scripts/Creature Scaling/creature_classlevelstats_buff.sql
+++ Scripts/Creature Scaling/creature_classlevelstats_buff.sql
@@ -1,21 +1,21 @@
--- Update levels 10 to 50 with incremental percentage increases
+-- Update levels 10 to 60 with incremental percentage increases
 UPDATE acore_world.creature_classlevelstats
 SET 
-    basehp0 = basehp0 * (1 + (level - 10) * 0.01),
-    basehp1 = basehp1 * (1 + (level - 10) * 0.01),
-    damage_base = damage_base * (1 + (level - 10) * 0.01),
-    damage_exp1 = damage_exp1 * (1 + (level - 10) * 0.01),
-    attackpower = attackpower * (1 + (level - 10) * 0.01),
-    rangedattackpower = rangedattackpower * (1 + (level - 10) * 0.01)
-WHERE level BETWEEN 10 AND 50;
+    basehp0 = basehp0 * (1 + (level - 10) * 0.004),
+    basehp1 = basehp1 * (1 + (level - 10) * 0.008),
+    damage_base = damage_base * (1 + (level - 10) * 0.004),
+    damage_exp1 = damage_exp1 * (1 + (level - 10) * 0.008),
+    attackpower = attackpower * (1 + (level - 10) * 0.008),
+    rangedattackpower = rangedattackpower * (1 + (level - 10) * 0.008)
+WHERE level BETWEEN 10 AND 60;
 
--- Update levels 51 and above with a flat 40% increase
+-- Update levels 61 and above with a flat 40% increase
 UPDATE acore_world.creature_classlevelstats
 SET 
-    basehp0 = basehp0 * 1.40,
+    basehp0 = basehp0 * 1.20,
     basehp1 = basehp1 * 1.40,
-    damage_base = damage_base * 1.40,
+    damage_base = damage_base * 1.20,
     damage_exp1 = damage_exp1 * 1.40,
     attackpower = attackpower * 1.40,
     rangedattackpower = rangedattackpower * 1.40
-WHERE level >= 51;
+WHERE level >= 61;
