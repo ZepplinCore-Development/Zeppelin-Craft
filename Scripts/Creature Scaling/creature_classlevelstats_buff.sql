b23e5595752fcec7f16caecb708bf4c53f5af8b7
justurn <justurn@live.com>
2024-08-18 13:17:38 +0800

    Damage Tweak

--- Scripts/Creature Scaling/creature_classlevelstats_buff.sql
+++ Scripts/Creature Scaling/creature_classlevelstats_buff.sql
@@ -9,13 +9,13 @@ SET
     rangedattackpower = rangedattackpower * (1 + (level - 10) * 0.008)
 WHERE level BETWEEN 10 AND 60;
 
--- Update levels 61 and above with a flat 40% increase
+-- Update levels 61 and above with a flat 20 - 40% increase
 UPDATE acore_world.creature_classlevelstats
 SET 
     basehp0 = basehp0 * 1.20,
     basehp1 = basehp1 * 1.40,
     damage_base = damage_base * 1.20,
     damage_exp1 = damage_exp1 * 1.40,
-    attackpower = attackpower * 1.40,
-    rangedattackpower = rangedattackpower * 1.40
+    attackpower = attackpower * 1.20,
+    rangedattackpower = rangedattackpower * 1.20
 WHERE level >= 61;
