bbe2928f75e4b14f1cfcfc4a1ba5f0eac26292f2
justurn <justurn@live.com>
2024-09-14 20:22:34 +0800

    Create zz_azeroth_flying.sql

---
+++ SQL/custom/db_world/General/Mounts/zz_azeroth_flying.sql
@@ -0,0 +1,6 @@
+UPDATE `spell`
+SET `AttributesEx4` = `AttributesEx4` & ~67108864
+WHERE (`EffectApplyAuraName1` = 78
+   OR `EffectApplyAuraName2` = 78
+   OR `EffectApplyAuraName3` = 78)
+  AND (`AttributesEx4` & 67108864) = 67108864;
