2e61be248a25c09799b80b584158bd0a97e7a84a
justurn <justurn@live.com>
2024-09-21 18:10:46 +0800

    Less bags :handbag:

--- SQL/custom/db_world/Zones/Outland/Shadowmoon Valley/zz_sludge_covered_object.sql
+++ SQL/custom/db_world/Zones/Outland/Shadowmoon Valley/zz_sludge_covered_object.sql
@@ -1,5 +1,4 @@
 -- Make the object always contain something
-
 DELETE FROM `item_loot_template` WHERE (`Entry` = 32724) AND (`Item` IN (32464, 32468, 32470, 32506, 32726));
 INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (32724, 32464, 0, 0, 0, 1, 0, 1, 5, 'Sludge-covered Object - Nethercite Ore'),
@@ -8,5 +7,10 @@ INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Quest
 (32724, 32506, 0, 0, 0, 1, 0, 1, 1, 'Sludge-covered Object - Netherwing Egg'),
 (32724, 32726, 0, 0, 0, 1, 0, 1, 1, 'Sludge-covered Object - Murkblood Escape Plans');
 
--- stack to 20 for QOL
+-- stacking up to 20 for QOL was 1
 UPDATE `item_template` SET `stackable` = 20 WHERE (`entry` = 32724);
+
+-- reduce drop chance from 50% to 20%
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 23286) AND (`Item` IN (32724));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(23286, 32724, 0, 20, 0, 1, 0, 1, 1, 'Black Blood of Draenor - Sludge-covered Object');
