afa256254e425bc36ab51d133f36955c57a41035
justurn <justurn@live.com>
2024-09-21 18:16:20 +0800

    Update ubuntustartup.sh

--- Scripts/ubuntustartup.sh
+++ Scripts/ubuntustartup.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 sleep 5
 
-gnome-terminal\
-	--tab\
-		--title="World Server" -- bash -c "cd; cd Zeppelin-Core; git pull; cd; cd azerothcore-wotlk/data/sql; rm -rf custom; cd; cp -r Zeppelin-Core/SQL/custom azerothcore-wotlk/data/sql; cd; azerothcore-wotlk/acore.sh run-worldserver; $SHELL"
-gnome-terminal\
-	--tab\
-		--title="Auth Server" -- bash -c "cd; azerothcore-wotlk/acore.sh run-authserver; $SHELL"
-		
-sleep 15
-		
-gnome-terminal\
-	--tab\
-		--title="Registration Bot" -- bash -c "cd; cd registrationbot && npm start; $SHELL"
-gnome-terminal\
-	--tab\
-		--title="Chat Bot" -- bash -c "cd; cd chatbot && ./run.sh; $SHELL"
-
+gnome-terminal \
+  --tab --title="World Server" -- bash -c "cd; cd Zeppelin-Core; git stash; git pull; git stash pop; cd; cd azerothcore-wotlk/data/sql; rm -rf custom; cd; cp -r Zeppelin-Core/SQL/custom azerothcore-wotlk/data/sql; cd; azerothcore-wotlk/acore.sh run-worldserver; $SHELL" \
+  --tab --title="Auth Server" -- bash -c "cd; azerothcore-wotlk/acore.sh run-authserver; $SHELL" \
+  --tab --title="Registration Bot" -- bash -c "cd; cd registrationbot && npm start; $SHELL" \
+  --tab --title="Chat Bot" -- bash -c "cd; cd chatbot && ./run.sh; $SHELL"
