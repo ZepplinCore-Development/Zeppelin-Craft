4c25d812a0cdabe296b0c193588791a835a3c79c
justurn <justurn@live.com>
2024-09-24 20:42:50 +0800

    start with a pick and a knife

--- SQL/custom/db_world/General/zz_starting_items.sql
+++ SQL/custom/db_world/General/zz_starting_items.sql
@@ -1,4 +1,4 @@
 REPLACE INTO acore_world.playercreateinfo_item (race,class,itemid,amount,Note) VALUES 
-(0,0,23720,1,'Riding Turtle'), 
-(0,0,40772,1,'Gnomish Army Knife'), 
+(0,0,7005,1,'Skinning Knife'), 
+(0,0,2901,1,'Mining Pick'), 
 (0,0,3762,2,'Librarian''s Satchel');
