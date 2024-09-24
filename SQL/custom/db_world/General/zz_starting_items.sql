d936afbcac9b42456ac162517dc07f8275e846a8
justurn <justurn@live.com>
2024-09-24 20:58:33 +0800

    fix starting items

--- SQL/custom/db_world/General/zz_starting_items.sql
+++ SQL/custom/db_world/General/zz_starting_items.sql
@@ -1,4 +1,5 @@
-REPLACE INTO acore_world.playercreateinfo_item (race,class,itemid,amount,Note) VALUES 
+DELETE FROM acore_world.playercreateinfo_item;
+INSERT INTO acore_world.playercreateinfo_item (race,class,itemid,amount,Note) VALUES 
 (0,0,7005,1,'Skinning Knife'), 
 (0,0,2901,1,'Mining Pick'), 
-(0,0,3762,2,'Librarian''s Satchel');
+(0,0,3762,1,'Librarian''s Satchel');
