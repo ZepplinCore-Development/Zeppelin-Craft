97d92c0f7cc23425aac8205f5987a5b3b281d3f7
justurn <justurn@live.com>
2024-09-27 21:11:11 +0800

    Update zz_sludge_covered_object.sql

--- SQL/custom/db_world/Zones/Outland/Shadowmoon Valley/zz_sludge_covered_object.sql
+++ SQL/custom/db_world/Zones/Outland/Shadowmoon Valley/zz_sludge_covered_object.sql
@@ -1,13 +1,17 @@
--- Make the object always contain something
-DELETE FROM `item_loot_template` WHERE (`Entry` = 32724) AND (`Item` IN (32464, 32468, 32470, 32506, 32726));
+-- Make the Sludge-covered Object always contain something, remove the escape plans
+DELETE FROM `item_loot_template` WHERE (`Entry` = 32724);
 INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (32724, 32464, 0, 0, 0, 1, 1, 1, 5, 'Sludge-covered Object - Nethercite Ore'),
 (32724, 32468, 0, 0, 0, 1, 1, 1, 5, 'Sludge-covered Object - Netherdust Pollen'),
 (32724, 32470, 0, 0, 0, 1, 1, 1, 5, 'Sludge-covered Object - Nethermine Flayer Hide'),
-(32724, 32506, 0, 0, 0, 1, 1, 1, 1, 'Sludge-covered Object - Netherwing Egg'),
-(32724, 32726, 0, 0, 0, 1, 1, 1, 1, 'Sludge-covered Object - Murkblood Escape Plans');
+(32724, 32506, 0, 0, 0, 1, 1, 1, 1, 'Sludge-covered Object - Netherwing Egg');
 
--- reduce drop chance from 50% to 20%
+-- reduce Sludge-covered Object drop chance from 50% to 20%
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 23286) AND (`Item` IN (32724));
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (23286, 32724, 0, 20, 0, 1, 0, 1, 1, 'Black Blood of Draenor - Sludge-covered Object');
+
+-- Move the escape plans to the Murkblood Foreman.
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 23305) AND (`Item` IN (32726));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(23305, 32726, 0, 50, 0, 1, 2, 1, 1, 'Crazed Murkblood Foreman - Murkblood Escape Plans');
