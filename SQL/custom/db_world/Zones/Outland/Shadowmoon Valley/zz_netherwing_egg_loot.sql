bea611761e78fea12fa9f396868b322ec85c1869
justurn <justurn@live.com>
2024-09-27 20:58:22 +0800

    Update zz_netherwing_egg_loot.sql

--- SQL/custom/db_world/Zones/Outland/Shadowmoon Valley/zz_netherwing_egg_loot.sql
+++ SQL/custom/db_world/Zones/Outland/Shadowmoon Valley/zz_netherwing_egg_loot.sql
@@ -1,2 +1,4 @@
 -- disable the force group loot flag
 UPDATE `gameobject_template` SET `Data15` = 0 WHERE (`entry` = 185915);
+-- disable BOP on the egg
+UPDATE `item_template` SET `bonding` = 0 WHERE (`entry` = 32506);
