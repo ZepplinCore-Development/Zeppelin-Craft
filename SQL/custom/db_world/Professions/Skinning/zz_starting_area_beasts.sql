cb36a75fc3492afb4c7fa2dcc12a0dcd224828a2
justurn <justurn@live.com>
2025-03-19 21:01:00 +0800

    updated indi prog queries

--- SQL/custom/db_world/Professions/Skinning/zz_starting_area_beasts.sql
+++ SQL/custom/db_world/Professions/Skinning/zz_starting_area_beasts.sql
@@ -3,14 +3,29 @@ DELETE FROM `skinning_loot_template` WHERE (`Entry` = 299);
 INSERT INTO `skinning_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (299, 2934, 0, 100, 0, 1, 0, 1, 3, '');
 
+-- AZUREMYST ISLE -------------------------------------------------------
+
+-- DUN MOROGH -------------------------------------------------------
+
+-- Ragged Young Wolf
+UPDATE `creature_template` SET `skinloot` = 299 WHERE `entry` = 705;
+
+-- TIRISTFALL GLADES -------------------------------------------------------
+
+
+-- ELWYNN FOREST -------------------------------------------------------
+
 -- Young Wolf
 UPDATE `creature_template` SET `skinloot` = 299 WHERE `entry` = 299;
 
 -- Timber Wolf
 UPDATE `creature_template` SET `skinloot` = 299 WHERE `entry` = 69;
 
--- Ragged Young Wolf
-UPDATE `creature_template` SET `skinloot` = 299 WHERE `entry` = 705;
+-- DUROTAR -------------------------------------------------------
+
+-- MULGORE -------------------------------------------------------
+
+-- TELDRASSIL -------------------------------------------------------
 
 -- Young Thistle Boar
 UPDATE `creature_template` SET `skinloot` = 299 WHERE `entry` = 1984;
@@ -23,3 +38,5 @@ UPDATE `creature_template` SET `skinloot` = 299 WHERE `entry` = 2031;
 
 -- Mangy Nightsaber
 UPDATE `creature_template` SET `skinloot` = 299 WHERE `entry` = 2032;
+
+-- EVERSONG WOODS -------------------------------------------------------
