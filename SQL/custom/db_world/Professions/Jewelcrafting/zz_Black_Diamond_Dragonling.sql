52ea6460028347c0b8326131817b7ea0d56fde90
justurn <justurn@live.com>
2025-03-21 08:22:27 +0800

    Update zz_Black_Diamond_Dragonling.sql

--- SQL/custom/db_world/Professions/Jewelcrafting/zz_Black_Diamond_Dragonling.sql
+++ SQL/custom/db_world/Professions/Jewelcrafting/zz_Black_Diamond_Dragonling.sql
@@ -39,7 +39,25 @@
         `spellid_1` = 91008,
         `spellcooldown_1` = 300000;
 
-DELETE FROM `creature_template` WHERE (`entry` = 1979);
-INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(1979, 0, 0, 0, 0, 0, 19295, 0, 0, 0, 'Black Diamond Dragonling', '', NULL, 0, 60, 60, 0, 35, 0, 1.11111, 0.857143, 1, 1, 18, 1, 0, 0, 3, 2000, 2000, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 1, '', 0, 1, 1, 1, 1.1, 0.5, 0, 100, 1, 8405008, 0, 0, 'npc_arcanite_dragonling', 12340);
+DELETE FROM `creature_template` 
+WHERE `entry` = 1979;
 
+INSERT INTO `creature_template` SET
+    `entry` = 1979,
+    `name` = 'Black Diamond Dragonling',
+    `minlevel` = 60,
+    `maxlevel` = 60,
+    `faction` = 35,
+    `speed_walk` = 1.11111,
+    `speed_run` = 0.857143,
+    `detection_range` = 18,
+    `DamageModifier` = 3,
+    `BaseAttackTime` = 2000,
+    `RangeAttackTime` = 2000,
+    `unit_class` = 1,
+    `unit_flags2` = 2048,
+    `type` = 9,
+    `movementId` = 100,
+    `mechanic_immune_mask` = 8405008,
+    `ScriptName` = 'npc_arcanite_dragonling',
+    `VerifiedBuild` = 12340;
