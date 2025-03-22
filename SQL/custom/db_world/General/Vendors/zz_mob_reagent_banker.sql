2f14500a9bb4697b6b10d0436c8b24cb1a259d88
justurn <justurn@live.com>
2025-03-22 20:45:52 +0800

    Update zz_mob_reagent_banker.sql

--- SQL/custom/db_world/General/Vendors/zz_mob_reagent_banker.sql
+++ SQL/custom/db_world/General/Vendors/zz_mob_reagent_banker.sql
@@ -1,8 +1,28 @@
 
 -- Reagent Banker - Template
 DELETE FROM `creature_template` WHERE (`entry` = 190011);
-INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(190011, 0, 0, 0, 0, 0, 20988, 0, 0, 0, 'Vitalik', 'Reagent Bank', NULL, 0, 6, 6, 0, 35, 1, 1, 1, 1, 1, 20, 1, 0, 0, 1, 2000, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 7, 138412032, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, 'npc_reagent_banker_account', NULL);
+INSERT INTO creature_template SET
+    `entry` = 190011,
+    `name` = 'Vitalik',
+    `subname` = 'Reagent Bank',
+    `minlevel` = 6,
+    `maxlevel` = 6,
+    `faction` = 35,
+    `npcflag` = 1,
+    `speed_run` = 1.0,
+    `BaseAttackTime` = 2000,
+    `unit_class` = 1,
+    `type` = 7,
+    `type_flags` = 138412032,
+    `flags_extra` = 2,
+    `ScriptName` = 'npc_reagent_banker_account';
+
+DELETE FROM creature_template_model WHERE `CreatureID` = 190011;
+INSERT INTO creature_template_model SET
+    `CreatureID` = '190011',
+    `idx` = 1,
+    `CreatureDisplayID` = '20988',
+    `probability` = 1.0;
 
 -- Reagent Banker - Spawns
 DELETE FROM `creature` WHERE (`id1` = 190011);
