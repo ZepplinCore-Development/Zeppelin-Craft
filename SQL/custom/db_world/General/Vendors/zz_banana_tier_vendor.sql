30efef616674d078d7abeb8469666854b0f38342
justurn <justurn@live.com>
2025-03-22 20:47:32 +0800

    Update zz_banana_tier_vendor.sql

--- SQL/custom/db_world/General/Vendors/zz_banana_tier_vendor.sql
+++ SQL/custom/db_world/General/Vendors/zz_banana_tier_vendor.sql
@@ -1,7 +1,29 @@
 -- Banana Creature Template
 DELETE FROM `creature_template` WHERE (`entry` = 9000010);
-INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(9000010, 0, 0, 0, 0, 0, 21265, 0, 0, 0, 'Banana', 'Efficiency Enthusiast', '', 0, 80, 80, 2, 1741, 128, 1, 1.14286, 1, 1, 20, 1, 0, 0, 1, 2000, 2000, 1, 1, 8, 768, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
+INSERT INTO `creature_template` SET
+    `entry` = 9000010,
+    `name` = 'Banana',
+    `subname` = 'Efficiency Enthusiast',
+    `IconName` = '',
+    `minlevel` = 80,
+    `maxlevel` = 80,
+    `exp` = 2,
+    `faction` = 1741,
+    `npcflag` = 128,
+    `BaseAttackTime` = 2000,
+    `RangeAttackTime` = 2000,
+    `unit_class` = 8,
+    `unit_flags` = 768,
+    `unit_flags2` = 2048,
+    `type` = 7,
+    `VerifiedBuild` = '12340';
+
+DELETE FROM `creature_template_model` WHERE `CreatureID` = 9000010;
+INSERT INTO `creature_template_model` SET
+    `CreatureID` = '9000010',
+    `idx` = 1,
+    `CreatureDisplayID` = '21265',
+    `probability` = 1.0;
 
 -- Banana Spawns
 DELETE FROM `creature` WHERE (`id1` = 9000010);
