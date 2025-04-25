de929a23a05ae05c4175776924c684248efcf303
justurn <justurn@live.com>
2025-04-25 20:39:35 +0800

    increased plant parts stack size

--- SQL/custom/db_world/General/Items/zz_increased_stack_sizes.sql
+++ SQL/custom/db_world/General/Items/zz_increased_stack_sizes.sql
@@ -1,38 +1,46 @@
--- Dark Iron Residue stacks to 2000
-UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 18945);
+-- 2000 Stack Size
 
--- Dark Iron Scraps stacks to 2000
-UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 22528);
+    -- Unidentified Plant Parts stacks to 2000
+    UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 24401);
 
--- Cryptfiend Parts stacks to 2000
-UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 22525);
+    -- Dark Iron Residue stacks to 2000
+    UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 18945);
 
--- Bone Fragments stacks to 2000
-UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 22526);
+    -- Dark Iron Scraps stacks to 2000
+    UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 22528);
 
--- Libram of Rumination stacks to 20, non unique
-UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11732);
+    -- Cryptfiend Parts stacks to 2000
+    UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 22525);
 
--- Libram of Constitution stacks to 20, non unique
-UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11733);
+    -- Bone Fragments stacks to 2000
+    UPDATE `item_template` SET `stackable` = 2000 WHERE (`entry` = 22526);
 
--- Libram of Tenacity stacks to 20, non unique
-UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11734);
+-- Unique to stackables
 
--- Libram of Resilience stacks to 20, non unique
-UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11736);
+    -- Libram of Rumination stacks to 20, non unique
+    UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11732);
 
--- Libram of Voracity stacks to 20, non unique
-UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11737);
+    -- Libram of Constitution stacks to 20, non unique
+    UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11733);
 
--- Libram of Rapidity stacks to 20, non unique
-UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 18332);
+    -- Libram of Tenacity stacks to 20, non unique
+    UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11734);
 
--- Libram of Focus stacks to 20, non unique
-UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 18333);
+    -- Libram of Resilience stacks to 20, non unique
+    UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11736);
 
--- Libram of Protection stacks to 20, non unique
-UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 18334);
+    -- Libram of Voracity stacks to 20, non unique
+    UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 11737);
 
--- Increases stack sizes to 200 where it is currently 20.
-UPDATE `item_template` SET `stackable` = 200 WHERE (`stackable` = 20);
+    -- Libram of Rapidity stacks to 20, non unique
+    UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 18332);
+
+    -- Libram of Focus stacks to 20, non unique
+    UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 18333);
+
+    -- Libram of Protection stacks to 20, non unique
+    UPDATE `item_template` SET `stackable` = 20, `maxcount` = 0 WHERE (`entry` = 18334);
+
+-- 200 base stack size
+    -- Universally increase stack sizes to 200 where it is currently 20.
+    UPDATE `item_template` SET `stackable` = 200 WHERE (`stackable` = 20);
