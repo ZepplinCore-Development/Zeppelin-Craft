698210617caff5020a5a5b959aa73d453ef705a4
justurn <justurn@live.com>
2025-05-05 13:07:46 +0800

    heroic dungeon dailies are not exclusive

--- SQL/custom/db_world/Dungeons/zz_daily_dungeon_heroic_rewards.sql
+++ SQL/custom/db_world/Dungeons/zz_daily_dungeon_heroic_rewards.sql
@@ -1,7 +1,8 @@
 -- Adds Azerite Shards to the reward table for Outland Heroic Dungeon Daily quests.
 UPDATE `quest_template` 
 SET `RewardItem2` = 58302, 
-    `RewardAmount2` = 4 
+    `RewardAmount2` = 4,
+    `ExclusiveGroup` = 0
 WHERE `ID` IN (
     SELECT `entry` 
     FROM `pool_quest` 
