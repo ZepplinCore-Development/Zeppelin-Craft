95a567ee30ac74d81ad4f06d57264669249fc553
justurn <justurn@live.com>
2025-05-05 21:16:11 +0800

    Update zz_increased_stack_sizes.sql

--- SQL/custom/db_world/General/Items/zz_increased_stack_sizes.sql
+++ SQL/custom/db_world/General/Items/zz_increased_stack_sizes.sql
@@ -48,3 +48,7 @@
 -- 1000 base stack size
     -- Universally increase stack sizes to 1000 where it is currently 100.
     UPDATE `item_template` SET `stackable` = 1000 WHERE (`stackable` = 100);
+
+-- 5 base stack size
+    -- Universally increase stack sizes to 50 where it is currently 5.
+    UPDATE `item_template` SET `stackable` = 50 WHERE (`stackable` = 5);
