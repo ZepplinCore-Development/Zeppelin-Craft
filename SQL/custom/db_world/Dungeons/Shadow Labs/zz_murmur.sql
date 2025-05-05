e92796b6d29a025186f8b11b81ba2e30fd2182d4
justurn <justurn@live.com>
2025-05-05 08:13:17 +0800

    Update zz_murmur.sql

--- SQL/custom/db_world/Dungeons/Shadow Labs/zz_murmur.sql
+++ SQL/custom/db_world/Dungeons/Shadow Labs/zz_murmur.sql
@@ -1,5 +1,5 @@
 -- New Murmur model to be Alakir (original diplay ID 18839)
-UPDATE `creature_template_model` SET `CreatureDisplayID` = 36062 WHERE `CreatureID` = 18708;
+UPDATE `creature_template_model` SET `CreatureDisplayID` = 36062, `DisplayScale` = 0.5 WHERE `CreatureID` = 18708;
 
 -- New creature_model_info entry
 DELETE FROM `creature_model_info` WHERE `DisplayID` = 36062;
@@ -7,7 +7,6 @@ INSERT INTO `creature_model_info` SET
     `DisplayID` = 36062,
     `BoundingRadius` = 1.5,
     `CombatReach` = 1.5,
-    `DisplayScale` = 0.5,
     `Gender` = 0,
     `DisplayID_Other_Gender` = 0;
 
