3fd4fcf309c1ead7f1b714dbeb4678ab52a832cf
justurn <justurn@live.com>
2025-05-29 20:28:05 +0800

    Update zz_molten_elemental.sql

--- SQL/db_world/Dungeons/Ragefire Chasm/zz_molten_elemental.sql
+++ SQL/db_world/Dungeons/Ragefire Chasm/zz_molten_elemental.sql
@@ -1,2 +1,5 @@
--- Remove fire immunity aura (7942) from Molten Elemental
+-- Remove Fire immunity aura (7942) from Molten Elemental
 UPDATE `creature_template_addon` SET `auras` = '' WHERE (`entry` = 11321);
+
+-- Remove Nature immune mask from Molten Elemental
+UPDATE `creature_template` SET `spell_school_immune_mask` = 0 WHERE (`entry` = 11321);
