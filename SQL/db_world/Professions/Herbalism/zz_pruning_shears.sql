4ebaa614220a4cbe2113cb601a59aab707c492fc
justurn <justurn@live.com>
2025-06-05 14:06:39 +0800

    Update zz_pruning_shears.sql

--- SQL/db_world/Professions/Herbalism/zz_pruning_shears.sql
+++ SQL/db_world/Professions/Herbalism/zz_pruning_shears.sql
@@ -66,7 +66,7 @@
         `spelltrigger_1` = 5,
         `bonding` = 1; -- BOP
 
--- Place all the hammer spell auras into the spell group table so they can be made exclusive
+-- Place all the shear spell auras into the spell group table so they can be made exclusive
     DELETE FROM `spell_group` WHERE `id` = 1116;
     INSERT INTO `spell_group` (`id`,`spell_id`,`special_flag`) VALUES
         (1116,91140,0), -- Journeyman
@@ -74,7 +74,79 @@
         (1116,91142,0), -- Master
         (1116,91143,0); -- Grand Master
 
--- Apply the exclusivity flag to the spell group for hammers
+-- Apply the exclusivity flag to the spell group for shears
     DELETE FROM `spell_group_stack_rules` WHERE `group_id` = 1116;
     INSERT INTO `spell_group_stack_rules` (`group_id`,`stack_rule`,`description`) VALUES
         (1116,1,'Group of Pruning Shears');
+
+-- Jouyneyman Shears Quest
+
+    SET @quest := 90008;
+
+    -- Quest Template
+        DELETE FROM `quest_template` WHERE `ID` = @quest;
+        INSERT INTO `quest_template` SET
+            `ID` = @quest,
+            `QuestLevel` = 18,
+            `MinLevel` = 10,
+            `QuestSortID` = 0,
+            `RewardXPDifficulty` = 5,
+            `Flags` = 136,
+            `RewardFactionID1` = 0,
+            `RewardFactionValue1` = 0,
+            `AllowableRaces` = 0,
+            `RequiredItemId1` = 2447, -- Peacebloom
+            `RequiredItemCount1` = 20,
+            `RequiredItemId2` = 765, -- Silverleaf
+            `RequiredItemCount2` = 20,
+            `LogTitle` = 'My First Pruning Shears',
+            `LogDescription` = 'Collect herbs to exchange for a pair of Journeyman Shears',
+            `QuestDescription` = 'You''ve been collecting herbs this whole time with you bare hands? like some kind of animal. This won''t do. I Tell you what, if you can fill my current herb order I''ll give you a pair of Pruning Shears.',
+            `QuestCompletionLog` = 'Return the herbs to exchange for a pair of Journeyman Shears',
+            `RewardItem1` = 57300,
+            `RewardAmount1` = 1,
+            `VerifiedBuild` = '0';
+
+    -- Quest Template Addon
+        DELETE FROM `quest_template_addon` WHERE (`ID` = @quest);
+        INSERT INTO `quest_template_addon` SET
+            `ID` = @quest,
+            `RequiredSkillID` = 182, -- Herbalism
+            `RequiredSkillPoints` = 75;
+
+    -- Quest Offer Reward
+        DELETE FROM `quest_offer_reward` WHERE (`ID` = @quest);
+        INSERT INTO `quest_offer_reward` SET
+            `ID` = @quest,
+            `RewardText` = 'get to work, those herbs won''t gather themselves';
+
+    -- Quest Request Items
+        DELETE FROM `quest_request_items` WHERE (`ID` = @quest);
+        INSERT INTO `quest_request_items` SET
+            `ID` = @quest,
+            `EmoteOnComplete` = 1,
+            `CompletionText` = 'Thanks for collecting the herb order';
+
+    -- Quest Giver Flag
+        UPDATE `creature_template` SET `npcflag` = 211 WHERE (`entry` = 16367); -- Botanist Tyniarrel
+        UPDATE `creature_template` SET `npcflag` = 83 WHERE (`entry` = 16644); -- Botanist Nathera 
+
+    -- Creature Quest Starter
+        DELETE FROM `creature_queststarter` WHERE (`quest` = @quest);
+        INSERT INTO `creature_queststarter` SET
+            `id` = 16367, -- Botanist Tyniarrel
+            `quest` = @quest;
+
+       INSERT INTO `creature_queststarter` SET
+            `id` = 16644, -- Botanist Nathera 
+            `quest` = @quest;
+
+    -- Creature Quest Ender
+        DELETE FROM `creature_questender` WHERE (`quest` = @quest);
+        INSERT INTO `creature_questender` SET
+            `id` = 16367,  -- Botanist Tyniarrel
+            `quest` = @quest;
+
+        INSERT INTO `creature_questender` SET
+            `id` = 16644, -- Botanist Nathera
+            `quest` = @quest;
